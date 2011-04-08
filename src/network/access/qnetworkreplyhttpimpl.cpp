c2b93b0d9ecc3f1efad1eb394c34252a2461f50b
Peter Hartmann <peter.hartmann@nokia.com>
2011-04-08 13:25:13 +0200

    fix build
    cregit: 0b7df2875409a1a164ea531b39a0a450dc4e0f02 https://github.com/qt/qtbase/commit/0b7df2875409a1a164ea531b39a0a450dc4e0f02

--- src/network/access/qnetworkreplyhttpimpl.cpp
+++ src/network/access/qnetworkreplyhttpimpl.cpp
@@ -73,6 +73,11 @@ end_include
 begin_include
 include|#
 directive|include
+file|"QtCore/qcoreapplication.h"
+end_include
+begin_include
+include|#
+directive|include
 file|"qnetworkcookiejar.h"
 end_include
 begin_ifndef
