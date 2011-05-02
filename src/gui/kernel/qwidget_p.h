6fbe486799996d7f3a9181c59334adb175dff1f7
Bjørn Erik Nilsen <bjorn.nilsen@nokia.com>
2011-05-02 12:39:45 +0200

    Fixes crash in QWidget::effectiveWinId.
    Widgets are left in a transitional (and incosistent) state while being
    re-parented, which caused QWidget::effectiveWinId() to crash in certain
    circumstances. See patch for more details.
    
    Auto test included.
    
    Reviewed-by: ogoffart
    (cherry picked from commit 6db0153cd7e35e4a919a76ae2aadbf2d2510bfb7)
    
    cregit: 1e41e3076aab37f151ffc09e6cd5768289e0d3e9 https://github.com/qt/qtbase/commit/1e41e3076aab37f151ffc09e6cd5768289e0d3e9

--- src/gui/kernel/qwidget_p.h
+++ src/gui/kernel/qwidget_p.h
@@ -3650,6 +3650,13 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+begin_decl_stmt
+name|uint
+name|inSetParent
+range|:
+literal|1
+decl_stmt|;
+end_decl_stmt
 begin_comment
 comment|// *************************** Platform specific ************************************
 end_comment
