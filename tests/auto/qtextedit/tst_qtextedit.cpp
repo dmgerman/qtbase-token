612520c9f0778ba62a1159c2fb96d3d0c8886295
Jason McDonald <jason.mcdonald@nokia.com>
2011-05-03 15:32:28 +1000

    Remove Q_ASSERT's from QTextEdit autotest
    Convert Q_ASSERT to QCOMPARE and remove mention of Q_ASSERT in comments
    to make it easier to check for unwanted Q_ASSERT's in the future.
    
    Change-Id: I7ae250428d4d5682806e724a7207523e65cd9851
    Task-number: QTBUG-17582
    Reviewed-by: Rohan McGovern
    (cherry picked from commit 3290dd0d3563310f6bdbabf74105367bd8c611d3)
    
    cregit: 6aca191e108bb518f0b9debf78707577664bf71c https://github.com/qt/qtbase/commit/6aca191e108bb518f0b9debf78707577664bf71c

--- tests/auto/qtextedit/tst_qtextedit.cpp
+++ tests/auto/qtextedit/tst_qtextedit.cpp
@@ -1328,7 +1328,7 @@ name|fontPointSize
 argument_list|()
 argument_list|)
 expr_stmt|;
-comment|// we currently Q_ASSERT_X in QFont::setPointSizeF for that
+comment|// we currently assert in QFont::setPointSizeF for that
 comment|//obj1.setFontPointSize(0.0);
 comment|//QCOMPARE(1.1, obj1.fontPointSize()); // Should not accept 0.0 => keep old
 comment|// int QTextEdit::fontWeight()
@@ -1369,7 +1369,7 @@ argument_list|()
 argument_list|)
 expr_stmt|;
 comment|// Range<1, 99>
-comment|/* Q_ASSERT_X in qfont.cpp     obj1.setFontWeight(INT_MIN);     QCOMPARE(1, obj1.fontWeight()); // Range<1, 99>     obj1.setFontWeight(INT_MAX);     QCOMPARE(99, obj1.fontWeight()); // Range<1, 99>     */
+comment|/* assertion in qfont.cpp     obj1.setFontWeight(INT_MIN);     QCOMPARE(1, obj1.fontWeight()); // Range<1, 99>     obj1.setFontWeight(INT_MAX);     QCOMPARE(99, obj1.fontWeight()); // Range<1, 99>     */
 comment|// bool QTextEdit::fontUnderline()
 comment|// void QTextEdit::setFontUnderline(bool)
 name|obj1
@@ -12458,13 +12458,13 @@ name|size
 argument_list|()
 argument_list|)
 expr_stmt|;
-name|Q_ASSERT
+name|QCOMPARE
 argument_list|(
 name|image
 operator|.
 name|depth
 argument_list|()
-operator|==
+argument_list|,
 literal|32
 argument_list|)
 expr_stmt|;
