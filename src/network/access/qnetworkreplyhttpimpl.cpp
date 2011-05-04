8a5fd7e1e79378e486a0de86a6cdb2c052741a0c
Peter Hartmann <peter.hartmann@nokia.com>
2011-05-04 14:33:21 +0200

    HTTP reply: do not load resources from cache that must be revalidated
    The header field "Cache-Control: must-revalidate" is a strict
    requirement for loading the resource from the server, and not reading it
    from the cache without revalidating first. With this patch, PreferCache
    will load such from the network instead of loading them from the cache,
    and AlwaysCache will throw a ContentNotFound error.
    
    Reviewed-by: Markus Goetz
    Task-number: QTBUG-18983
    
    cregit: 0c9f327102d30fa3b37559469445da6f7de1f6ff https://github.com/qt/qtbase/commit/0c9f327102d30fa3b37559469445da6f7de1f6ff

--- src/network/access/qnetworkreplyhttpimpl.cpp
+++ src/network/access/qnetworkreplyhttpimpl.cpp
@@ -1986,15 +1986,6 @@ name|lastModified
 argument_list|)
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|CacheLoadControlAttribute
-operator|==
-name|QNetworkRequest
-operator|::
-name|PreferNetwork
-condition|)
-block|{
 name|it
 operator|=
 name|cacheHeaders
@@ -2043,7 +2034,6 @@ condition|)
 return|return
 literal|false
 return|;
-block|}
 block|}
 name|QDateTime
 name|currentDateTime
