e17ced8374bff486678ff8b36a4815bc32508215
Rohan McGovern <rohan.mcgovern@nokia.com>
2011-05-18 10:26:00 +1000

    Remove Q_ASSERT from qsslsocket autotest.
    Make the server thread exit early if the socket is invalid.  This will
    make the test fail gracefully instead of crashing.
    
    Change-Id: Ia9564c94dd32d65e6e9bdb4a2410f1512409546c
    Task-number: QTBUG-17582
    Reviewed-by: Rohan McGovern
    (cherry picked from commit b9b3ced2eafe13e491d5af5ad7432733941bfd8d)
    
    Conflicts:
    
    	tests/auto/qsslsocket/tst_qsslsocket.cpp
    
    cregit: d7e10b2a71f1fa196472544309dc59488229ebeb https://github.com/qt/qtbase/commit/d7e10b2a71f1fa196472544309dc59488229ebeb

--- tests/auto/qsslsocket/tst_qsslsocket.cpp
+++ tests/auto/qsslsocket/tst_qsslsocket.cpp
@@ -9450,19 +9450,19 @@ name|server
 operator|.
 name|socket
 decl_stmt|;
-name|QVERIFY
-argument_list|(
+if|if
+condition|(
+operator|!
 name|socket
-argument_list|)
-expr_stmt|;
-name|QVERIFY
-argument_list|(
+operator|||
+operator|!
 name|socket
 operator|->
 name|isValid
 argument_list|()
-argument_list|)
-expr_stmt|;
+condition|)
+return|return;
+comment|// error
 name|socket
 operator|->
 name|ignoreSslErrors
