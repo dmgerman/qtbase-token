28b98b7232cef6cece2bd9d28b94c404dd67d6df
Mark Brand <mabrand@mabrand.nl>
2011-07-03 22:45:15 +0200

    add missing multiple inclusion guard
    Change-Id: I82e9d1ddfc1a46bf47b8948dce800dcbcc1a0305
    Reviewed-by: Robin Burchell <robin+qt@viroteck.net>
    Reviewed-by: Constantin Makshin <cmakshin@gmail.com>
    Reviewed-by: hjk <qthjk@ovi.com>
    
    cregit: 621b121c07bcbc5639d3c412224496489d41fad4 https://github.com/qt/qtbase/commit/621b121c07bcbc5639d3c412224496489d41fad4

--- src/plugins/codecs/kr/cp949codetbl.h
+++ src/plugins/codecs/kr/cp949codetbl.h
@@ -2,6 +2,17 @@ begin_unit
 begin_comment
 comment|/**************************************************************************** ** ** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies). ** All rights reserved. ** Contact: Nokia Corporation (qt-info@nokia.com) ** ** This file is part of the plugins of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** GNU Lesser General Public License Usage ** This file may be used under the terms of the GNU Lesser General Public ** License version 2.1 as published by the Free Software Foundation and ** appearing in the file LICENSE.LGPL included in the packaging of this ** file. Please review the following information to ensure the GNU Lesser ** General Public License version 2.1 requirements will be met: ** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Nokia gives you certain additional ** rights. These rights are described in the Nokia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU General ** Public License version 3.0 as published by the Free Software Foundation ** and appearing in the file LICENSE.GPL included in the packaging of this ** file. Please review the following information to ensure the GNU General ** Public License version 3.0 requirements will be met: ** http://www.gnu.org/copyleft/gpl.html. ** ** Other Usage ** Alternatively, this file may be used in accordance with the terms and ** conditions contained in a signed written agreement between you and Nokia. ** ** ** ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
 end_comment
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|CP949CODETBL_H
+end_ifndef
+begin_define
+DECL|macro|CP494CODETBL_H
+define|#
+directive|define
+name|CP494CODETBL_H
+end_define
 begin_decl_stmt
 DECL|variable|cp949_icode_to_unicode
 specifier|static
@@ -17658,4 +17669,11 @@ literal|0xd7a3
 block|}
 decl_stmt|;
 end_decl_stmt
+begin_endif
+endif|#
+directive|endif
+end_endif
+begin_comment
+comment|// CP494CODETBL_H
+end_comment
 end_unit
