75fdc81e45d7601bd54783cecba462f6808cf4db
Gunnar Sletta <gunnar.sletta@nokia.com>
2011-08-31 14:07:54 +0200

    The XCB plugin should not rely on QT_CONFIG=opengl anymore
    Change-Id: Ieeedb7eb68704df811c613873bd2fc4b77e2aa04
    Reviewed-on: http://codereview.qt.nokia.com/3966
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Kevin Simons <kevin.simons@nokia.com>
    Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
    
    cregit: 9cdf9973c60f6974fe9b405b10ef9cffef7ae4f2 https://github.com/qt/qtbase/commit/9cdf9973c60f6974fe9b405b10ef9cffef7ae4f2

--- src/plugins/platforms/xcb/qglxintegration.cpp
+++ src/plugins/platforms/xcb/qglxintegration.cpp
@@ -15,11 +15,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<QGLFormat>
-end_include
-begin_include
-include|#
-directive|include
 file|"qxcbwindow.h"
 end_include
 begin_include
