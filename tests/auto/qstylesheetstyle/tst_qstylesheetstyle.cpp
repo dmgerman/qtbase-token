5d28f6ab1b29b0c7bd6cedeb0a65e49c100c5e53
Caroline Chao <caroline.chao@nokia.com>
2011-09-07 14:56:47 +0200

    Change in tst_qstylesheetstyle.cpp to work around a bug in testcocoon parser.
    Change-Id: I1361e4c02e0c8a0424957c531e22a7b2236f725d
    Reviewed-on: http://codereview.qt.nokia.com/4360
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@nokia.com>
    
    cregit: e77032c4e6c802f8e2397eb3c9d8daf1ee64266c https://github.com/qt/qtbase/commit/e77032c4e6c802f8e2397eb3c9d8daf1ee64266c

--- tests/auto/qstylesheetstyle/tst_qstylesheetstyle.cpp
+++ tests/auto/qstylesheetstyle/tst_qstylesheetstyle.cpp
@@ -10135,7 +10135,10 @@ name|tst_QStyleSheetStyle
 operator|::
 name|QTBUG15910_crashNullWidget
 parameter_list|()
-block|{     struct :
+block|{
+struct|struct
+name|Widget
+super|:
 name|QWidget
 block|{
 specifier|virtual
@@ -10219,7 +10222,7 @@ expr_stmt|;
 block|}
 block|}
 name|w
-expr_stmt|;
+struct|;
 name|w
 operator|.
 name|setStyleSheet
