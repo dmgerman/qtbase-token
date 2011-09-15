143c1e400ef176c24a56cbc767d18dffa4a666be
Tobias Hunger <tobias.hunger@gmx.de>
2011-09-15 11:29:34 +0200

    Fix warning when building objects with a Q_OBJECT macro
    Do not trigger a self-assign warning when running into code containing
    a Q_OBJECT macro. Currently this happens a lot e.g. when using clang
    to build code using Qt.
    
    Change-Id: I68995624b18406f337318599e463f36f87486e66
    Reviewed-on: http://codereview.qt-project.org/4960
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: hjk <qthjk@ovi.com>
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>
    
    cregit: 1416f4c595d6078c08f93483695e0b64c7fbb2a7 https://github.com/qt/qtbase/commit/1416f4c595d6078c08f93483695e0b64c7fbb2a7

--- src/corelib/kernel/qobjectdefs.h
+++ src/corelib/kernel/qobjectdefs.h
@@ -338,7 +338,7 @@ define|#
 directive|define
 name|Q_OBJECT_CHECK
 define|\
-value|template<typename T> inline void qt_check_for_QOBJECT_macro(const T&_q_argument) const \     { int i = qYouForgotTheQ_OBJECT_Macro(this,&_q_argument); i = i; }
+value|template<typename T> inline void qt_check_for_QOBJECT_macro(const T&_q_argument) const \     { int i = qYouForgotTheQ_OBJECT_Macro(this,&_q_argument); i = i + 1; }
 end_define
 begin_expr_stmt
 name|template
