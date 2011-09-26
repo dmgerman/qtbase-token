873b7ae2a03494d2a4f96ac9ed8b36ef32d0ef5e
Lars Knoll <lars.knoll@nokia.com>
2011-09-26 22:59:46 +0200

    Implement support for QGuiApp::setOverrideCursor()
    Properly support application override cursors and move
    the documentation into the proper place.
    
    Change-Id: I0198b38e0d2e7b10f0fcae342b468e7f8341bf02
    Reviewed-on: http://codereview.qt-project.org/5562
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Paul Olav Tvete <paul.tvete@nokia.com>
    
    cregit: 84251f84b6e7deadb7d50527b877d13cea74ad4b https://github.com/qt/qtbase/commit/84251f84b6e7deadb7d50527b877d13cea74ad4b

--- src/widgets/kernel/qapplication.cpp
+++ src/widgets/kernel/qapplication.cpp
@@ -18542,12 +18542,6 @@ begin_comment
 comment|/*!     \fn void QApplication::beep()      Sounds the bell, using the default volume and sound. The function is \e not     available in Qt for Embedded Linux. */
 end_comment
 begin_comment
-comment|/*!     \fn void QApplication::setOverrideCursor(const QCursor&cursor)      Sets the application override cursor to \a cursor.      Application override cursors are intended for showing the user that the     application is in a special state, for example during an operation that     might take some time.      This cursor will be displayed in all the application's widgets until     restoreOverrideCursor() or another setOverrideCursor() is called.      Application cursors are stored on an internal stack. setOverrideCursor()     pushes the cursor onto the stack, and restoreOverrideCursor() pops the     active cursor off the stack. changeOverrideCursor() changes the curently     active application override cursor.      Every setOverrideCursor() must eventually be followed by a corresponding     restoreOverrideCursor(), otherwise the stack will never be emptied.      Example:     \snippet doc/src/snippets/code/src_gui_kernel_qapplication_x11.cpp 0      \sa overrideCursor(), restoreOverrideCursor(), changeOverrideCursor(),     QWidget::setCursor() */
-end_comment
-begin_comment
-comment|/*!     \fn void QApplication::restoreOverrideCursor()      Undoes the last setOverrideCursor().      If setOverrideCursor() has been called twice, calling     restoreOverrideCursor() will activate the first cursor set. Calling this     function a second time restores the original widgets' cursors.      \sa setOverrideCursor(), overrideCursor() */
-end_comment
-begin_comment
 comment|/*!     \macro qApp     \relates QApplication      A global pointer referring to the unique application object. It is     equivalent to the pointer returned by the QCoreApplication::instance()     function except that, in GUI applications, it is a pointer to a     QApplication instance.      Only one application object can be created.      \sa QCoreApplication::instance() */
 end_comment
 begin_ifndef
