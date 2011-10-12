26df6ac0328c472781148f8e1eb4614be6fad7a9
Lars Knoll <lars.knoll@nokia.com>
2011-10-12 22:36:41 +0200

    Less friends is good, at least for C++ code :)
    Change-Id: Ia46359ee80eb30b8e16a02b7d5376cc9610fac84
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>
    
    cregit: 5ad123c13694da8c5ae417f9b7b8f96fd47170ef https://github.com/qt/qtbase/commit/5ad123c13694da8c5ae417f9b7b8f96fd47170ef

--- src/corelib/kernel/qcoreevent.h
+++ src/corelib/kernel/qcoreevent.h
@@ -1056,12 +1056,6 @@ end_decl_stmt
 begin_decl_stmt
 name|friend
 name|class
-name|Q3AccelManager
-decl_stmt|;
-end_decl_stmt
-begin_decl_stmt
-name|friend
-name|class
 name|QShortcutMap
 decl_stmt|;
 end_decl_stmt
