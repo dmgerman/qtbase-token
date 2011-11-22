452900515207f8b233812d2872ef2fd51ba5807b
Samuel Rødal <samuel.rodal@nokia.com>
2011-11-22 10:53:40 +0100

    Add a note in the QScreen documentation about logical vs physical DPI.
    Change-Id: I1d536711cbbc0ea8a21c6917e5e0066c1ad4ccae
    Reviewed-by: Alan Alpert <alan.alpert@nokia.com>
    
    cregit: 11bf824c03d07a2c4ac7f1d614351c532d7a52d6 https://github.com/qt/qtbase/commit/11bf824c03d07a2c4ac7f1d614351c532d7a52d6

--- src/gui/kernel/qscreen.cpp
+++ src/gui/kernel/qscreen.cpp
@@ -51,7 +51,7 @@ block|}
 class|;
 end_class
 begin_comment
-comment|/*!     \class QScreen     \brief The QScreen class is used to query screen properties.      \inmodule QtGui */
+comment|/*!     \class QScreen     \brief The QScreen class is used to query screen properties.      A note on logical vs physical dots per inch: physical DPI is based on the     actual physical pixel sizes when available, and is useful for print preview     and other cases where it's desirable to know the exact physical dimensions     of screen displayed contents.      Logical dots per inch are used to convert font and user interface elements     from point sizes to pixel sizes, and might be different from the physical     dots per inch. The logical dots per inch are sometimes user-settable in the     desktop environment's settings panel, to let the user globally control UI     and font sizes in different applications.      \inmodule QtGui */
 end_comment
 begin_constructor
 DECL|function|QScreen
