5f5b76de02fd9bf73f1ce375b50f3f899f492365
Bradley T. Hughes <bradley.hughes@nokia.com>
2012-01-17 12:51:46 +0100

    Skip tst_QMenu::QTBUG_10735_crashWithDialog() on Mac OS X
    This test currently hangs, preventing the autotest from finishing.
    
    Task-number: QTBUG-23677
    Change-Id: I3c5f56c10735d65fb35de4e22a1e2e6414532309
    Reviewed-by: Jason McDonald <jason.mcdonald@nokia.com>
    
    cregit: 6f21509ac006cf8cd82d3b35b1016a4f9a0c0f89 https://github.com/qt/qtbase/commit/6f21509ac006cf8cd82d3b35b1016a4f9a0c0f89

--- tests/auto/widgets/widgets/qmenu/tst_qmenu.cpp
+++ tests/auto/widgets/widgets/qmenu/tst_qmenu.cpp
@@ -5977,6 +5977,16 @@ operator|::
 name|QTBUG_10735_crashWithDialog
 parameter_list|()
 block|{
+ifdef|#
+directive|ifdef
+name|Q_OS_MAC
+name|QSKIP
+argument_list|(
+literal|"Test currently hangs on Mac OS X, see QTBUG-23677"
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
 name|MyMenu
 name|menu
 decl_stmt|;
