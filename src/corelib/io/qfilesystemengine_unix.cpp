ac17094d2b35941e4d9a0b85b518f553ec822d0f
Nick Ratelle <nratelle@qnx.com>
2012-01-24 17:26:13 -0500

    realpath(X,0) IS supported on QNX.
    Adds check for Q_OS_QNX to force
    QFileSystemEntry QFileSystemEngine::canonicalName() use realpath(X, 0) on QNX
    as well.
    
    Change-Id: Id0a32277e6d043753c42101c91a393ebedb48a0a
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: ee592fc042efc19acbc5407f5277abe7372c138d https://github.com/qt/qtbase/commit/ee592fc042efc19acbc5407f5277abe7372c138d

--- src/corelib/io/qfilesystemengine_unix.cpp
+++ src/corelib/io/qfilesystemengine_unix.cpp
@@ -827,6 +827,12 @@ argument_list|(
 name|Q_OS_MAC
 argument_list|)
 operator|&&
+operator|!
+name|defined
+argument_list|(
+name|Q_OS_QNX
+argument_list|)
+operator|&&
 name|_POSIX_VERSION
 operator|<
 literal|200809L
