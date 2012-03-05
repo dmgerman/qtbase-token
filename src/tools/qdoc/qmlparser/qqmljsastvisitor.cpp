118e85bedc629f620737c7d958bdccf8bfe502f7
Lars Knoll <lars.knoll@nokia.com>
2012-03-05 15:34:40 +0100

    Move qdoc into qtbase and bootstrap it
    We need qdoc in qtbase to be able to properly
    modularize our documentation and build it
    when building the different Qt modules.
    
    qdoc does contain a copy of the qml parser from
    qmldevtools, but this is the lesser evil compared
    to how we are currently forced to genereate our
    docs (and the fact that no developer can run
    qdoc and check the docs for their module).
    
    Change-Id: I9f748459382a11cf5d5153d1ee611d7a5d3f4ac1
    Reviewed-by: Casper van Donderen <casper.vandonderen@nokia.com>
    Reviewed-by: Martin Smith <martin.smith@nokia.com>
    
    cregit: 448a3cfe17735499cb43186200e8d52669b6a7ca https://github.com/qt/qtbase/commit/448a3cfe17735499cb43186200e8d52669b6a7ca

---
+++ src/tools/qdoc/qmlparser/qqmljsastvisitor.cpp
@@ -0,0 +1,44 @@
+begin_unit
+begin_comment
+comment|/**************************************************************************** ** ** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies). ** Contact: http://www.qt-project.org/ ** ** This file is part of the QtQml module of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** GNU Lesser General Public License Usage ** This file may be used under the terms of the GNU Lesser General Public ** License version 2.1 as published by the Free Software Foundation and ** appearing in the file LICENSE.LGPL included in the packaging of this ** file. Please review the following information to ensure the GNU Lesser ** General Public License version 2.1 requirements will be met: ** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Nokia gives you certain additional ** rights. These rights are described in the Nokia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU General ** Public License version 3.0 as published by the Free Software Foundation ** and appearing in the file LICENSE.GPL included in the packaging of this ** file. Please review the following information to ensure the GNU General ** Public License version 3.0 requirements will be met: ** http://www.gnu.org/copyleft/gpl.html. ** ** Other Usage ** Alternatively, this file may be used in accordance with the terms and ** conditions contained in a signed written agreement between you and Nokia. ** ** ** ** ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
+end_comment
+begin_include
+include|#
+directive|include
+file|"qqmljsastvisitor_p.h"
+end_include
+begin_macro
+name|QT_QML_BEGIN_NAMESPACE
+end_macro
+begin_namespace
+DECL|namespace|QQmlJS
+DECL|namespace|AST
+namespace|namespace
+name|QQmlJS
+block|{
+namespace|namespace
+name|AST
+block|{
+DECL|function|Visitor
+name|Visitor
+operator|::
+name|Visitor
+parameter_list|()
+block|{ }
+DECL|function|~Visitor
+name|Visitor
+operator|::
+name|~
+name|Visitor
+parameter_list|()
+block|{ }
+block|}
+block|}
+end_namespace
+begin_comment
+comment|// namespace QQmlJS::AST
+end_comment
+begin_macro
+name|QT_QML_END_NAMESPACE
+end_macro
+end_unit
