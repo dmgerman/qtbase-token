d43c41481c935b22a95105ad0f2a6d9848ff7758
Shane Kearns <ext-shane.2.kearns@nokia.com>
2012-03-22 16:24:17 +0000

    Add support for PUT & POST to qget test
    Due to some bugs that are not reproducable with a normal HTTP GET
    This patch also adds the option to process multiple URLs
    serially (using application level queuing) rather than the default
    parallel (using QNetworkAccessManager queuing on 6 TCP connections)
    & renames the authentication command line options to match wget.
    
    Change-Id: I10915feb3bba23abbd7a72f9844c03f347f9bff5
    Reviewed-by: Richard J. Moore <rich@kde.org>
    Reviewed-by: Martin Petersson <Martin.Petersson@nokia.com>
    
    cregit: b08f74f08939be310a79b5efcb5721e35c5deb21 https://github.com/qt/qtbase/commit/b08f74f08939be310a79b5efcb5721e35c5deb21

--- tests/manual/qnetworkaccessmanager/qget/qget.h
+++ tests/manual/qnetworkaccessmanager/qget/qget.h
@@ -35,7 +35,7 @@ file|<QFile>
 end_include
 begin_decl_stmt
 name|class
-name|DownloadItem
+name|TransferItem
 range|:
 name|public
 name|QObject
@@ -43,11 +43,118 @@ block|{
 name|Q_OBJECT
 name|public
 operator|:
-name|DownloadItem
+expr|enum
+name|Method
+block|{
+name|Get
+block|,
+name|Put
+block|,
+name|Post
+block|}
+block|;
+name|TransferItem
 argument_list|(
+argument|const QNetworkRequest&r
+argument_list|,
+argument|const QString&u
+argument_list|,
+argument|const QString&p
+argument_list|,
+argument|QNetworkAccessManager&n
+argument_list|,
+argument|Method m
+argument_list|)
+block|;
+name|void
+name|start
+argument_list|()
+block|;
+name|signals
+operator|:
+name|void
+name|downloadFinished
+argument_list|(
+name|TransferItem
+operator|*
+name|self
+argument_list|)
+block|;
+name|public
+name|slots
+operator|:
+name|void
+name|progress
+argument_list|(
+name|qint64
+argument_list|,
+name|qint64
+argument_list|)
+block|;
+name|public
+operator|:
+name|Method
+name|method
+block|;
+name|QNetworkRequest
+name|request
+block|;
 name|QNetworkReply
 operator|*
+name|reply
+block|;
+name|QNetworkAccessManager
+operator|&
+name|nam
+block|;
+name|QFile
+operator|*
+name|inputFile
+block|;
+name|QFile
+operator|*
+name|outputFile
+block|;
+name|QList
+operator|<
+name|QUrl
+operator|>
+name|redirects
+block|;
+name|QString
+name|user
+block|;
+name|QString
+name|password
+block|; }
+decl_stmt|;
+end_decl_stmt
+begin_decl_stmt
+name|class
+name|DownloadItem
+range|:
+name|public
+name|TransferItem
+block|{
+name|Q_OBJECT
+name|public
+operator|:
+name|DownloadItem
+argument_list|(
+specifier|const
+name|QNetworkRequest
+operator|&
 name|r
+argument_list|,
+specifier|const
+name|QString
+operator|&
+name|user
+argument_list|,
+specifier|const
+name|QString
+operator|&
+name|password
 argument_list|,
 name|QNetworkAccessManager
 operator|&
@@ -57,25 +164,6 @@ block|;
 operator|~
 name|DownloadItem
 argument_list|()
-block|;
-name|QNetworkReply
-operator|*
-name|networkReply
-argument_list|()
-block|{
-return|return
-name|reply
-return|;
-block|}
-name|signals
-operator|:
-name|void
-name|downloadFinished
-argument_list|(
-name|DownloadItem
-operator|*
-name|self
-argument_list|)
 block|;
 name|private
 name|slots
@@ -90,22 +178,44 @@ argument_list|()
 block|;
 name|private
 operator|:
-name|QNetworkAccessManager
-operator|&
-name|nam
+block|}
+decl_stmt|;
+end_decl_stmt
+begin_decl_stmt
+name|class
+name|UploadItem
+range|:
+name|public
+name|TransferItem
+block|{
+name|Q_OBJECT
+name|public
+operator|:
+name|UploadItem
+argument_list|(
+argument|const QNetworkRequest&r
+argument_list|,
+argument|const QString&user
+argument_list|,
+argument|const QString&password
+argument_list|,
+argument|QNetworkAccessManager&nam
+argument_list|,
+argument|QFile *f
+argument_list|,
+argument|TransferItem::Method method
+argument_list|)
 block|;
-name|QNetworkReply
-operator|*
-name|reply
+operator|~
+name|UploadItem
+argument_list|()
 block|;
-name|QFile
-name|file
-block|;
-name|QList
-operator|<
-name|QUrl
-operator|>
-name|redirects
+name|private
+name|slots
+operator|:
+name|void
+name|finished
+argument_list|()
 block|; }
 decl_stmt|;
 end_decl_stmt
@@ -133,12 +243,38 @@ specifier|const
 name|QUrl
 operator|&
 name|url
+argument_list|,
+specifier|const
+name|QString
+operator|&
+name|user
+argument_list|,
+specifier|const
+name|QString
+operator|&
+name|password
+argument_list|)
+block|;
+name|void
+name|upload
+argument_list|(
+argument|const QUrl&url
+argument_list|,
+argument|const QString&user
+argument_list|,
+argument|const QString&password
+argument_list|,
+argument|const QString&filename
+argument_list|,
+argument|const QString&contentType
+argument_list|,
+argument|TransferItem::Method method
 argument_list|)
 block|;
 name|void
 name|setProxy
 argument_list|(
-argument|const QNetworkProxy& proxy
+argument|const QNetworkProxy&proxy
 argument_list|)
 block|{
 name|nam
@@ -149,29 +285,9 @@ name|proxy
 argument_list|)
 block|; }
 name|void
-name|setHttpUser
-argument_list|(
-argument|const QString& user
-argument_list|)
-block|{
-name|httpUser
-operator|=
-name|user
-block|; }
-name|void
-name|setHttpPassword
-argument_list|(
-argument|const QString& password
-argument_list|)
-block|{
-name|httpPassword
-operator|=
-name|password
-block|; }
-name|void
 name|setProxyUser
 argument_list|(
-argument|const QString& user
+argument|const QString&user
 argument_list|)
 block|{
 name|proxyUser
@@ -181,13 +297,27 @@ block|; }
 name|void
 name|setProxyPassword
 argument_list|(
-argument|const QString& password
+argument|const QString&password
 argument_list|)
 block|{
 name|proxyPassword
 operator|=
 name|password
 block|; }
+expr|enum
+name|QueueMode
+block|{
+name|Parallel
+block|,
+name|Serial
+block|}
+block|;
+name|void
+name|setQueueMode
+argument_list|(
+argument|QueueMode mode
+argument_list|)
+block|;
 name|public
 name|slots
 operator|:
@@ -231,9 +361,6 @@ operator|*
 name|authenticator
 argument_list|)
 block|;
-ifndef|#
-directive|ifndef
-name|QT_NO_SSL
 name|void
 name|sslErrors
 argument_list|(
@@ -250,39 +377,43 @@ operator|&
 name|errors
 argument_list|)
 block|;
-endif|#
-directive|endif
 name|void
 name|downloadFinished
 argument_list|(
-name|DownloadItem
+name|TransferItem
 operator|*
 name|item
 argument_list|)
 block|;
 name|private
 operator|:
+name|TransferItem
+operator|*
+name|findTransfer
+argument_list|(
+name|QNetworkReply
+operator|*
+name|reply
+argument_list|)
+block|;
 name|QNetworkAccessManager
 name|nam
 block|;
 name|QList
 operator|<
-name|DownloadItem
+name|TransferItem
 operator|*
 operator|>
-name|downloads
-block|;
-name|QString
-name|httpUser
-block|;
-name|QString
-name|httpPassword
+name|transfers
 block|;
 name|QString
 name|proxyUser
 block|;
 name|QString
 name|proxyPassword
+block|;
+name|QueueMode
+name|queueMode
 block|; }
 decl_stmt|;
 end_decl_stmt
