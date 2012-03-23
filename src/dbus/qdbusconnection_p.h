bd4cb593b5e8575805d1307b917f9e93011bf32f
Jan Arne Petersen <jpetersen@openismus.com>
2012-03-23 13:58:04 +0100

    Fix QDBusServer with more than one connection
    Create a new QDBusConnectionPrivate for every new connection in
    qDBusNewConnection instead of creating a single QDBusConnectionPrivate
    in the QDBusServer constructor which gets assigned the latest connected
    DBusConnection in qDBusNewConnection (and loses track on all previous
    DBusConnections).
    
    Also extend tst_QDBusConnection::registerObjectPeer() test with multiple
    connections to the server.
    
    Task-Number: 24921
    Change-Id: I4341e8d48d464f3fe0a314a6ab14f848545d65a0
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: a386194f9952683c0be5028f2b7f0ce9617fe404 https://github.com/qt/qtbase/commit/a386194f9952683c0be5028f2b7f0ce9617fe404

--- src/dbus/qdbusconnection_p.h
+++ src/dbus/qdbusconnection_p.h
@@ -1552,6 +1552,11 @@ begin_comment
 comment|// this connection's base service
 end_comment
 begin_decl_stmt
+name|QStringList
+name|serverConnectionNames
+decl_stmt|;
+end_decl_stmt
+begin_decl_stmt
 name|ConnectionMode
 name|mode
 decl_stmt|;
