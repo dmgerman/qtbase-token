432c60bcca8d1100e23c2b260f9d93170ecb3b38
Kent Hansen <kent.hansen@nokia.com>
2012-04-22 14:35:56 +0200

    Port QNetworkSession to QMetaMethod-based connectNotify()
    The const char *-based API is deprecated and will be removed in Qt5.
    
    Change-Id: I36f6dc761e3b5a087e38db29b761c3e9237958b4
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Shane Kearns <shane.kearns@accenture.com>
    
    cregit: e525957253de61b9f2f7066e222fa8f06a6afacc https://github.com/qt/qtbase/commit/e525957253de61b9f2f7066e222fa8f06a6afacc

--- src/network/bearer/qnetworksession.h
+++ src/network/bearer/qnetworksession.h
@@ -330,8 +330,8 @@ name|void
 name|connectNotify
 argument_list|(
 specifier|const
-name|char
-operator|*
+name|QMetaMethod
+operator|&
 name|signal
 argument_list|)
 block|;
@@ -340,8 +340,8 @@ name|void
 name|disconnectNotify
 argument_list|(
 specifier|const
-name|char
-operator|*
+name|QMetaMethod
+operator|&
 name|signal
 argument_list|)
 block|;
