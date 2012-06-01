403c553293b65329f66f7e9f600f082d4b42980c
Lars Knoll <lars.knoll@nokia.com>
2012-06-01 23:09:31 +0200

    Add proper collation support to Qt
    QString::localeAwareCompare() has always been a broken
    way to support collation. The current implementation is
    not even thread safe.
    
    This adds a proper collation class that fixes the problems
    and finally allows Qt to sort properly according to locale
    rules.
    
    The class is private for now, but is intendent to be made
    public with 5.1
    
    Change-Id: Idb4e75ff68a398c9813af622af884a90898d2be9
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 1e9be1327bed93542816abb1aef6bcee25216ce0 https://github.com/qt/qtbase/commit/1e9be1327bed93542816abb1aef6bcee25216ce0

--- src/corelib/tools/qstring.h
+++ src/corelib/tools/qstring.h
@@ -6739,6 +6739,12 @@ name|class
 name|QByteArray
 decl_stmt|;
 end_decl_stmt
+begin_decl_stmt
+name|friend
+name|class
+name|QCollator
+decl_stmt|;
+end_decl_stmt
 begin_macro
 name|friend
 end_macro
