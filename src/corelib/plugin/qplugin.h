f4128f4da9f2279354ac47aa27de0471790b8193
Lars Knoll <lars.knoll@nokia.com>
2012-06-12 11:43:45 +0200

    Make sure you can link against more than one static plugin
    The old macro was leading to symbol clashes.
    
    Change-Id: I090c511d4090bc96fc6c88537fae7bbe7f143b6c
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Jason McDonald <jason.mcdonald@nokia.com>
    
    cregit: f9c9d8aaa6684f596cf5675592782d5397e52093 https://github.com/qt/qtbase/commit/f9c9d8aaa6684f596cf5675592782d5397e52093

--- src/corelib/plugin/qplugin.h
+++ src/corelib/plugin/qplugin.h
@@ -250,7 +250,7 @@ parameter_list|(
 name|PLUGINCLASS
 parameter_list|)
 define|\
-value|static QT_PREPEND_NAMESPACE(QObject) *qt_plugin_instance() \     Q_PLUGIN_INSTANCE(PLUGINCLASS) \     static const char *qt_plugin_query_metadata() { return (const char *)qt_pluginMetaData; } \     const QT_PREPEND_NAMESPACE(QStaticPlugin) qt_static_plugin_##PLUGINCLASS() { \         QT_PREPEND_NAMESPACE(QStaticPlugin) plugin = { qt_plugin_instance, qt_plugin_query_metadata }; \         return plugin; \     }
+value|static QT_PREPEND_NAMESPACE(QObject) *qt_plugin_instance_##PLUGINCLASS() \     Q_PLUGIN_INSTANCE(PLUGINCLASS) \     static const char *qt_plugin_query_metadata_##PLUGINCLASS() { return (const char *)qt_pluginMetaData; } \     const QT_PREPEND_NAMESPACE(QStaticPlugin) qt_static_plugin_##PLUGINCLASS() { \         QT_PREPEND_NAMESPACE(QStaticPlugin) plugin = { qt_plugin_instance_##PLUGINCLASS, qt_plugin_query_metadata_##PLUGINCLASS}; \         return plugin; \     }
 end_define
 begin_else
 else|#
