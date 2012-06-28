f7ead183cd3ba8becafcb280c4f854f0c23c7565
Samuel Rødal <samuel.rodal@nokia.com>
2012-06-28 10:27:00 +0200

    Removed private header warning from public QOpenGLPaintDevice header.
    Change-Id: I9f2068d7271778e98251d8cbcfffa3a8c3e58f3d
    Reviewed-by: Kim M. Kalland <kim.kalland@nokia.com>
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    
    cregit: 32c54642dd9af1f1bf48c259f55dad6d5339c5e8 https://github.com/qt/qtbase/commit/32c54642dd9af1f1bf48c259f55dad6d5339c5e8

--- src/gui/opengl/qopenglpaintdevice.h
+++ src/gui/opengl/qopenglpaintdevice.h
@@ -18,36 +18,6 @@ ifndef|#
 directive|ifndef
 name|QT_NO_OPENGL
 end_ifndef
-begin_comment
-comment|//
-end_comment
-begin_comment
-comment|//  W A R N I N G
-end_comment
-begin_comment
-comment|//  -------------
-end_comment
-begin_comment
-comment|//
-end_comment
-begin_comment
-comment|// This file is not part of the Qt API.  It exists for the convenience
-end_comment
-begin_comment
-comment|// of the QtGui module.  This header file may change from
-end_comment
-begin_comment
-comment|// version to version without notice, or even be removed.
-end_comment
-begin_comment
-comment|//
-end_comment
-begin_comment
-comment|// We mean it.
-end_comment
-begin_comment
-comment|//
-end_comment
 begin_include
 include|#
 directive|include
