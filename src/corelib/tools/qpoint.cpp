8f020cd3077e128d434e1816a397331abaf91155
Laszlo Papp <lpapp@kde.org>
2012-07-08 12:10:13 +0100

    Make the documentation consistent with the header and QVector* classes
    Change-Id: If0c5951029beb388cf4b1b8269de7305d0556ef1
    Reviewed-by: Casper van Donderen <casper.vandonderen@nokia.com>
    
    cregit: a91e65036ece7caf22fa4ccda3e974bc07f2072f https://github.com/qt/qtbase/commit/a91e65036ece7caf22fa4ccda3e974bc07f2072f

--- src/corelib/tools/qpoint.cpp
+++ src/corelib/tools/qpoint.cpp
@@ -22,7 +22,7 @@ name|QT_BEGIN_NAMESPACE
 comment|/*!     \class QPoint     \ingroup painting      \brief The QPoint class defines a point in the plane using integer     precision.      A point is specified by a x coordinate and an y coordinate which     can be accessed using the x() and y() functions. The isNull()     function returns true if both x and y are set to 0. The     coordinates can be set (or altered) using the setX() and setY()     functions, or alternatively the rx() and ry() functions which     return references to the coordinates (allowing direct     manipulation).      Given a point \e p, the following statements are all equivalent:      \snippet code/src_corelib_tools_qpoint.cpp 0      A QPoint object can also be used as a vector: Addition and     subtraction are defined as for vectors (each component is added     separately). A QPoint object can also be divided or multiplied by     an \c int or a \c qreal.      In addition, the QPoint class provides the manhattanLength()     function which gives an inexpensive approximation of the length of     the QPoint object interpreted as a vector. Finally, QPoint objects     can be streamed as well as compared.      \sa QPointF, QPolygon */
 comment|/*****************************************************************************   QPoint member functions  *****************************************************************************/
 comment|/*!     \fn QPoint::QPoint()      Constructs a null point, i.e. with coordinates (0, 0)      \sa isNull() */
-comment|/*!     \fn QPoint::QPoint(int x, int y)      Constructs a point with the given coordinates (\a x, \a  y).      \sa setX(), setY() */
+comment|/*!     \fn QPoint::QPoint(int xpos, int ypos)      Constructs a point with the given coordinates (\a xpos, \a ypos).      \sa setX(), setY() */
 comment|/*!     \fn bool QPoint::isNull() const      Returns true if both the x and y coordinates are set to 0,     otherwise returns false. */
 comment|/*!     \fn int QPoint::x() const      Returns the x coordinate of this point.      \sa setX(), rx() */
 comment|/*!     \fn int QPoint::y() const      Returns the y coordinate of this point.      \sa setY(), ry() */
@@ -329,7 +329,7 @@ begin_comment
 comment|/*!     \fn QPointF::QPointF(const QPoint&point)      Constructs a copy of the given \a point.      \sa toPoint() */
 end_comment
 begin_comment
-comment|/*!     \fn QPointF::QPointF(qreal x, qreal y)      Constructs a point with the given coordinates (\a x, \a y).      \sa setX(), setY() */
+comment|/*!     \fn QPointF::QPointF(qreal xpos, qreal ypos)      Constructs a point with the given coordinates (\a xpos, \a ypos).      \sa setX(), setY() */
 end_comment
 begin_comment
 comment|/*!     \fn bool QPointF::isNull() const      Returns true if both the x and y coordinates are set to +0.0;     otherwise returns false.      \note Since this function treats +0.0 and -0.0 differently, points     with zero-valued coordinates where either or both values have a     negative sign are not defined to be null points. */
