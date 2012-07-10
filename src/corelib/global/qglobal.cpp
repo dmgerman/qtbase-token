1ab3d121f96e649dbe17f02126f607781c2d4c78
Marc Mutz <marc.mutz@kdab.com>
2012-07-10 15:30:06 +0200

    QtGlobal: document Q_DECL_FINAL and Q_DECL_OVERRIDE
    Change-Id: I9b292ae3319c30ad878aade4416fb88155465a54
    Reviewed-by: David Faure <faure@kde.org>
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    
    cregit: c09eeff2ab97111fc91b708bc8a9d981e4ccc990 https://github.com/qt/qtbase/commit/c09eeff2ab97111fc91b708bc8a9d981e4ccc990

--- src/corelib/global/qglobal.cpp
+++ src/corelib/global/qglobal.cpp
@@ -3370,6 +3370,12 @@ end_comment
 begin_comment
 comment|/*!     \macro Q_DECL_NOEXCEPT_EXPR(x)     \relates<QtGlobal>     \since 5.0      This macro marks a function as non-throwing if \a x is true. If     the function does nevertheless throw, the behaviour is defined:     std::terminate() is called.      The macro expands to C++11 noexcept(x), if available, or to     nothing otherwise.      If you need the always-true version of C++11 noexcept, use     Q_DECL_NOEXCEPT.      If you don't need C++11 noexcept semantics, e.g. because your     function can't possibly throw, don't use this macro, use     Q_DECL_NOTHROW instead.      \sa Q_DECL_NOTHROW, Q_DECL_NOEXCEPT_EXPR */
 end_comment
+begin_comment
+comment|/*!     \macro Q_DECL_OVERRIDE     \since 5.0     \relates<QtGlobal>      This macro can be used to declare an overriding virtual     function. Use of this markup will allow the compiler to generate     an error if the overriding virtual function does not in fact     override anything.      It expands to "override" if your compiler supports that C++11     contextual keyword, or to nothing otherwise.      The macro goes at the end of the function, usually after the     \c{const}, if any:     \code     // generate error if this doesn't actually override anything:     virtual void MyWidget::paintEvent(QPaintEvent*) Q_DECL_OVERRIDE;     \endcode      \sa Q_DECL_FINAL */
+end_comment
+begin_comment
+comment|/*!     \macro Q_DECL_FINAL     \since 5.0     \relates<QtGlobal>      This macro can be used to declare an overriding virtual or a class     as "final", with Java semantics. Further-derived classes can then     no longer override this virtual function, or inherit from this     class, respectively.      It expands to "final" if your compiler supports that C++11     contextual keyword, or something non-standard if your compiler     supports something close enough to the C++11 semantics, or to     nothing otherwise.      The macro goes at the end of the function, usually after the     \c{const}, if any:     \code     // more-derived classes no longer permitted to override this:     virtual void MyWidget::paintEvent(QPaintEvent*) Q_DECL_FINAL;     \endcode      For classes, it goes in front of the \c{:} in the class     definition, if any:     \code     class QRect Q_DECL_FINAL { // cannot be derived from         // ...     };     \endcode      \sa Q_DECL_OVERRIDE */
+end_comment
 begin_macro
 name|QT_END_NAMESPACE
 end_macro
