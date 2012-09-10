57a13ba7384b839aa801d732472702b0c149f28f
Sérgio Martins <sergio.martins@kdab.com>
2012-09-10 19:53:01 +0100

    Fix QT_NO_SYSTEMTRAYICON build.
    The ifndef was added to the header but was missing in
    the implementation file.
    
    Change-Id: I25111b34b9c087465306ef81c23d6667257b5e71
    Reviewed-by: Qt Doc Bot <qt_docbot@qt-project.org>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>
    
    cregit: 260da7a832182698c4ca872c4634fbab02c4880c https://github.com/qt/qtbase/commit/260da7a832182698c4ca872c4634fbab02c4880c

--- src/gui/kernel/qplatformsystemtrayicon_qpa.cpp
+++ src/gui/kernel/qplatformsystemtrayicon_qpa.cpp
@@ -7,6 +7,11 @@ include|#
 directive|include
 file|"qplatformsystemtrayicon.h"
 end_include
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|QT_NO_SYSTEMTRAYICON
+end_ifndef
 begin_function
 name|QT_BEGIN_NAMESPACE
 comment|/*!     \class QPlatformSystemTrayIcon     \brief The QPlatformSystemTrayIcon class abstracts the system tray icon and interaction.      \sa QSystemTray */
@@ -73,4 +78,11 @@ include|#
 directive|include
 file|"moc_qplatformsystemtrayicon.cpp"
 end_include
+begin_endif
+endif|#
+directive|endif
+end_endif
+begin_comment
+comment|// QT_NO_SYSTEMTRAYICON
+end_comment
 end_unit
