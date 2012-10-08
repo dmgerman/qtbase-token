a561bf5d6e4207bed3476fbfb1b0f9d642b9d796
Joerg Bornemann <joerg.bornemann@digia.com>
2012-10-08 14:36:58 +0200

    remove superfluous include from echoplugin example
    Change-Id: I8a53c5e628e43796d7f4b53c311dd4d200527262
    Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>
    
    cregit: 43ea7154184243b78c52f40c6ed344495f1386e9 https://github.com/qt/qtbase/commit/43ea7154184243b78c52f40c6ed344495f1386e9

--- examples/tools/echoplugin/plugin/echoplugin.h
+++ examples/tools/echoplugin/plugin/echoplugin.h
@@ -26,11 +26,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"echoplugin.h"
-end_include
-begin_include
-include|#
-directive|include
 file|"echointerface.h"
 end_include
 begin_comment
