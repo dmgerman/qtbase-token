447ae8c20da4400a710235eca324d5b70a544d0c
Sergio Ahumada <sergio.ahumada@digia.com>
2012-10-22 12:17:48 +0200

    test: Don't fail if the network test server is not set up
    Not having access to a network test server is not a failure per se
    but rather an enviromental condition not met at run-time.
    
    Change-Id: Ie7d10ca5fbf2df45fb1fd1ac19718c9fae855c03
    Reviewed-by: Caroline Chao <caroline.chao@digia.com>
    
    cregit: 704a4e4747b2c42e262d9b4bd440ff365ab92a35 https://github.com/qt/qtbase/commit/704a4e4747b2c42e262d9b4bd440ff365ab92a35

--- tests/auto/network/access/qabstractnetworkcache/tst_qabstractnetworkcache.cpp
+++ tests/auto/network/access/qabstractnetworkcache/tst_qabstractnetworkcache.cpp
@@ -349,12 +349,17 @@ operator|::
 name|initTestCase
 parameter_list|()
 block|{
-name|QVERIFY
-argument_list|(
+if|if
+condition|(
+operator|!
 name|QtNetworkSettings
 operator|::
 name|verifyTestNetworkSettings
 argument_list|()
+condition|)
+name|QSKIP
+argument_list|(
+literal|"No network test server available"
 argument_list|)
 expr_stmt|;
 ifndef|#
