b28b8c576d85570d56081563618e0f79adeee731
Michele Caini <michele.caini@gmail.com>
2012-10-23 22:39:35 +0200

    Review of documentation.
    Documentation has been updated, changes apply to Qt5 as well as Qt4.
    
    Change-Id: I13241a3b4c16d2cb1b24f80fe26832467621923a
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    
    cregit: b0bc4f3279074f429078cb5a27f577d4e12bd854 https://github.com/qt/qtbase/commit/b0bc4f3279074f429078cb5a27f577d4e12bd854

--- src/corelib/thread/qreadwritelock.cpp
+++ src/corelib/thread/qreadwritelock.cpp
@@ -41,7 +41,7 @@ begin_macro
 name|QT_BEGIN_NAMESPACE
 end_macro
 begin_comment
-comment|/*! \class QReadWriteLock     \inmodule QtCore     \brief The QReadWriteLock class provides read-write locking.      \threadsafe      \ingroup thread      A read-write lock is a synchronization tool for protecting     resources that can be accessed for reading and writing. This type     of lock is useful if you want to allow multiple threads to have     simultaneous read-only access, but as soon as one thread wants to     write to the resource, all other threads must be blocked until     the writing is complete.      In many cases, QReadWriteLock is a direct competitor to QMutex.     QReadWriteLock is a good choice if there are many concurrent     reads and writing occurs infrequently.      Example:      \snippet code/src_corelib_thread_qreadwritelock.cpp 0      To ensure that writers aren't blocked forever by readers, readers     attempting to obtain a lock will not succeed if there is a blocked     writer waiting for access, even if the lock is currently only     accessed by other readers. Also, if the lock is accessed by a     writer and another writer comes in, that writer will have     priority over any readers that might also be waiting.      Like QMutex, a QReadWriteLock can be recursively locked by the     same thread when constructed in     \l{QReadWriteLock::RecursionMode}. In such cases,     unlock() must be called the same number of times lockForWrite() or     lockForRead() was called. Note that the lock type cannot be     changed when trying to lock recursively, i.e. it is not possible     to lock for reading in a thread that already has locked for     writing (and vice versa).      \sa QReadLocker, QWriteLocker, QMutex, QSemaphore */
+comment|/*! \class QReadWriteLock     \inmodule QtCore     \brief The QReadWriteLock class provides read-write locking.      \threadsafe      \ingroup thread      A read-write lock is a synchronization tool for protecting     resources that can be accessed for reading and writing. This type     of lock is useful if you want to allow multiple threads to have     simultaneous read-only access, but as soon as one thread wants to     write to the resource, all other threads must be blocked until     the writing is complete.      In many cases, QReadWriteLock is a direct competitor to QMutex.     QReadWriteLock is a good choice if there are many concurrent     reads and writing occurs infrequently.      Example:      \snippet code/src_corelib_thread_qreadwritelock.cpp 0      To ensure that writers aren't blocked forever by readers, readers     attempting to obtain a lock will not succeed if there is a blocked     writer waiting for access, even if the lock is currently only     accessed by other readers. Also, if the lock is accessed by a     writer and another writer comes in, that writer will have     priority over any readers that might also be waiting.      Like QMutex, a QReadWriteLock can be recursively locked by the     same thread when constructed with \l{QReadWriteLock::Recursive} as     \l{QReadWriteLock::RecursionMode}. In such cases,     unlock() must be called the same number of times lockForWrite() or     lockForRead() was called. Note that the lock type cannot be     changed when trying to lock recursively, i.e. it is not possible     to lock for reading in a thread that already has locked for     writing (and vice versa).      \sa QReadLocker, QWriteLocker, QMutex, QSemaphore */
 end_comment
 begin_comment
 comment|/*!      \enum QReadWriteLock::RecursionMode     \since 4.4      \value Recursive In this mode, a thread can lock the same     QReadWriteLock multiple times and the mutex won't be unlocked     until a corresponding number of unlock() calls have been made.      \value NonRecursive In this mode, a thread may only lock a     QReadWriteLock once.      \sa QReadWriteLock() */
@@ -86,7 +86,7 @@ expr_stmt|;
 block|}
 end_destructor
 begin_comment
-comment|/*!     Locks the lock for reading. This function will block the current     thread if any thread (including the current) has locked for     writing.      \sa unlock(), lockForWrite(), tryLockForRead() */
+comment|/*!     Locks the lock for reading. This function will block the current     thread if another thread has locked for writing.      It is not possible to lock for read if the thread already has     locked for write.      \sa unlock(), lockForWrite(), tryLockForRead() */
 end_comment
 begin_function
 DECL|function|lockForRead
@@ -260,7 +260,7 @@ expr_stmt|;
 block|}
 end_function
 begin_comment
-comment|/*!     Attempts to lock for reading. If the lock was obtained, this     function returns true, otherwise it returns false instead of     waiting for the lock to become available, i.e. it does not block.      The lock attempt will fail if another thread has locked for     writing.      If the lock was obtained, the lock must be unlocked with unlock()     before another thread can successfully lock it.      \sa unlock(), lockForRead() */
+comment|/*!     Attempts to lock for reading. If the lock was obtained, this     function returns true, otherwise it returns false instead of     waiting for the lock to become available, i.e. it does not block.      The lock attempt will fail if another thread has locked for     writing.      If the lock was obtained, the lock must be unlocked with unlock()     before another thread can successfully lock it for writing.      It is not possible to lock for read if the thread already has     locked for write.      \sa unlock(), lockForRead() */
 end_comment
 begin_function
 DECL|function|tryLockForRead
@@ -414,7 +414,7 @@ return|;
 block|}
 end_function
 begin_comment
-comment|/*! \overload      Attempts to lock for reading. This function returns true if the     lock was obtained; otherwise it returns false. If another thread     has locked for writing, this function will wait for at most \a     timeout milliseconds for the lock to become available.      Note: Passing a negative number as the \a timeout is equivalent to     calling lockForRead(), i.e. this function will wait forever until     lock can be locked for reading when \a timeout is negative.      If the lock was obtained, the lock must be unlocked with unlock()     before another thread can successfully lock it.      \sa unlock(), lockForRead() */
+comment|/*! \overload      Attempts to lock for reading. This function returns true if the     lock was obtained; otherwise it returns false. If another thread     has locked for writing, this function will wait for at most \a     timeout milliseconds for the lock to become available.      Note: Passing a negative number as the \a timeout is equivalent to     calling lockForRead(), i.e. this function will wait forever until     lock can be locked for reading when \a timeout is negative.      If the lock was obtained, the lock must be unlocked with unlock()     before another thread can successfully lock it for writing.      It is not possible to lock for read if the thread already has     locked for write.      \sa unlock(), lockForRead() */
 end_comment
 begin_function
 DECL|function|tryLockForRead
@@ -618,7 +618,7 @@ return|;
 block|}
 end_function
 begin_comment
-comment|/*!     Locks the lock for writing. This function will block the current     thread if another thread has locked for reading or writing.      \sa unlock(), lockForRead(), tryLockForWrite() */
+comment|/*!     Locks the lock for writing. This function will block the current     thread if another thread (including the current) has locked for     reading or writing (unless the lock has been created using the     \l{QReadWriteLock::Recursive} mode).      It is not possible to lock for write if the thread already has     locked for read.      \sa unlock(), lockForRead(), tryLockForWrite() */
 end_comment
 begin_function
 DECL|function|lockForWrite
@@ -753,7 +753,7 @@ expr_stmt|;
 block|}
 end_function
 begin_comment
-comment|/*!     Attempts to lock for writing. If the lock was obtained, this     function returns true; otherwise, it returns false immediately.      The lock attempt will fail if another thread has locked for     reading or writing.      If the lock was obtained, the lock must be unlocked with unlock()     before another thread can successfully lock it.      \sa unlock(), lockForWrite() */
+comment|/*!     Attempts to lock for writing. If the lock was obtained, this     function returns true; otherwise, it returns false immediately.      The lock attempt will fail if another thread has locked for     reading or writing.      If the lock was obtained, the lock must be unlocked with unlock()     before another thread can successfully lock it.      It is not possible to lock for write if the thread already has     locked for read.      \sa unlock(), lockForWrite() */
 end_comment
 begin_function
 DECL|function|tryLockForWrite
@@ -872,7 +872,7 @@ return|;
 block|}
 end_function
 begin_comment
-comment|/*! \overload      Attempts to lock for writing. This function returns true if the     lock was obtained; otherwise it returns false. If another thread     has locked for reading or writing, this function will wait for at     most \a timeout milliseconds for the lock to become available.      Note: Passing a negative number as the \a timeout is equivalent to     calling lockForWrite(), i.e. this function will wait forever until     lock can be locked for writing when \a timeout is negative.      If the lock was obtained, the lock must be unlocked with unlock()     before another thread can successfully lock it.      \sa unlock(), lockForWrite() */
+comment|/*! \overload      Attempts to lock for writing. This function returns true if the     lock was obtained; otherwise it returns false. If another thread     has locked for reading or writing, this function will wait for at     most \a timeout milliseconds for the lock to become available.      Note: Passing a negative number as the \a timeout is equivalent to     calling lockForWrite(), i.e. this function will wait forever until     lock can be locked for writing when \a timeout is negative.      If the lock was obtained, the lock must be unlocked with unlock()     before another thread can successfully lock it.      It is not possible to lock for write if the thread already has     locked for read.      \sa unlock(), lockForWrite() */
 end_comment
 begin_function
 DECL|function|tryLockForWrite
