a087be3d4e99b014373e93ca402482e1ea204018
Samuel Rødal <samuel.rodal@digia.com>
2012-11-09 13:14:33 +0100

    Added private header warnings in src/gui/kernel
    Change-Id: If3ad9d1e18543639f9e6abb8899b72772bc70c83
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    
    cregit: f9d122159e48f73a2b6e7df2e48ba470ccc87d77 https://github.com/qt/qtbase/commit/f9d122159e48f73a2b6e7df2e48ba470ccc87d77

--- src/gui/kernel/qplatforminputcontext_p.h
+++ src/gui/kernel/qplatforminputcontext_p.h
@@ -13,6 +13,36 @@ define|#
 directive|define
 name|QPLATFORMINPUTCONTEXT_P_H
 end_define
+begin_comment
+comment|//
+end_comment
+begin_comment
+comment|//  W A R N I N G
+end_comment
+begin_comment
+comment|//  -------------
+end_comment
+begin_comment
+comment|//
+end_comment
+begin_comment
+comment|// This file is not part of the Qt API.  It exists purely as an
+end_comment
+begin_comment
+comment|// implementation detail.  This header file may change from version to
+end_comment
+begin_comment
+comment|// version without notice, or even be removed.
+end_comment
+begin_comment
+comment|//
+end_comment
+begin_comment
+comment|// We mean it.
+end_comment
+begin_comment
+comment|//
+end_comment
 begin_include
 include|#
 directive|include
