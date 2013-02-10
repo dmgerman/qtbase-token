31330246af235b3832781de67df3db21ac20d6a6
Darryl L. Miles <darryl.miles@darrylmiles.org>
2013-02-10 07:18:42 +0000

    Json writer, only emit floating point finite numbers, ignore INF/NaN
    My interpretation of RFC4627, Section 2.4 "Numbers" of:
      Numeric values that cannot be represented as sequences of digits
       (such as Infinity and NaN) are not permitted.
    
    I have also verified this matter with NodeJS JSON.stringify() that
    emitting a null is consistent behavior with a JSON implementation
    written in JavaScript.
    
    Previously Qt would emit:
    {
     plusInfinity: inf,
     minusInfinity: -inf,
     notANumber: nan
    }
    
    Which maybe turned into a string values of "inf", "-inf", "nan" by
    the receiving parser.  Now it returns the JSON null value just like
    NodeJS JSON.stringify().
    
    Change-Id: I9f9c17f12b2606280806c47a9d90465c4ba5f786
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: e2d614b6a9e88c90331e3de9bb339a2ed609ea95 https://github.com/qt/qtbase/commit/e2d614b6a9e88c90331e3de9bb339a2ed609ea95

--- src/corelib/json/qjson_p.h
+++ src/corelib/json/qjson_p.h
@@ -81,6 +81,11 @@ end_include
 begin_include
 include|#
 directive|include
+file|<qnumeric.h>
+end_include
+begin_include
+include|#
+directive|include
 file|<limits.h>
 end_include
 begin_decl_stmt
