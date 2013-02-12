101a12a0b23e9dece65546e02f3a40716db8eb3e
Friedemann Kleint <Friedemann.Kleint@digia.com>
2013-02-12 15:12:06 +0100

    Fix developer-build of qsslsocket_onDemandCertificates_member.
    Do not include qsslsocket_p.h unless SSL is present.
    
    Change-Id: I7e56b7758729907892d85f97d5a9d3ccaf7a3314
    Reviewed-by: Peter Hartmann <phartmann@rim.com>
    
    cregit: 9a74bbf34f16c68e74baac253183bc0a8a23a981 https://github.com/qt/qtbase/commit/9a74bbf34f16c68e74baac253183bc0a8a23a981

--- tests/auto/network/ssl/qsslsocket_onDemandCertificates_member/tst_qsslsocket_onDemandCertificates_member.cpp
+++ tests/auto/network/ssl/qsslsocket_onDemandCertificates_member/tst_qsslsocket_onDemandCertificates_member.cpp
@@ -32,6 +32,11 @@ include|#
 directive|include
 file|"private/qhostinfo_p.h"
 end_include
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|QT_NO_OPENSSL
+end_ifndef
 begin_include
 include|#
 directive|include
@@ -41,6 +46,16 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+begin_comment
+comment|// !QT_NO_OPENSSL
+end_comment
+begin_endif
+endif|#
+directive|endif
+end_endif
+begin_comment
+comment|// QT_BUILD_INTERNAL
+end_comment
 begin_include
 include|#
 directive|include
