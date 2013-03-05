54e98987745558b02a22670e3017513c773f9059
Samuel Rødal <samuel.rodal@digia.com>
2013-03-05 15:43:50 +0100

    Fixed dashes being rendered differently depending on system clip.
    We need to clip lines to the unclipped device rect in the case of
    dashing, since otherwise the dashes will be shifted and rendered
    differently when partial repaints are done.
    
    Task-number: QTBUG-24762
    Change-Id: I3599b54baa552acc20bf8cc2e12f846b45f6019e
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>
    Reviewed-by: Gunnar Sletta <gunnar.sletta@digia.com>
    
    cregit: a12f6ba302e54c1570c54aa4c722f2dafbf794af https://github.com/qt/qtbase/commit/a12f6ba302e54c1570c54aa4c722f2dafbf794af

--- src/gui/painting/qcosmeticstroker.cpp
+++ src/gui/painting/qcosmeticstroker.cpp
@@ -1630,10 +1630,22 @@ operator|>>
 literal|2
 expr_stmt|;
 block|}
+comment|// dashes are sensitive to clips, so we need to clip consistently when painting to the same device
+name|QRect
+name|clipRect
+init|=
+name|strokeSelection
+operator|&
+name|Dashed
+condition|?
+name|deviceRect
+else|:
+name|clip
+decl_stmt|;
 comment|// setup FP clip bounds
 name|xmin
 operator|=
-name|clip
+name|clipRect
 operator|.
 name|left
 argument_list|()
@@ -1642,7 +1654,7 @@ literal|1
 expr_stmt|;
 name|xmax
 operator|=
-name|clip
+name|clipRect
 operator|.
 name|right
 argument_list|()
@@ -1651,7 +1663,7 @@ literal|2
 expr_stmt|;
 name|ymin
 operator|=
-name|clip
+name|clipRect
 operator|.
 name|top
 argument_list|()
@@ -1660,7 +1672,7 @@ literal|1
 expr_stmt|;
 name|ymax
 operator|=
-name|clip
+name|clipRect
 operator|.
 name|bottom
 argument_list|()
