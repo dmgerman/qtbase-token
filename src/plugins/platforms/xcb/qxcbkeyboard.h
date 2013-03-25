6aa4b828b08b5e4874ada8fd42fc5a675efea7ea
Gatis Paeglis <gatis.paeglis@digia.com>
2013-03-25 14:44:48 +0100

    Utilize libxkbcommon API for the keyboard backend on X11
    libxkbcommon is a keymap compiler [1] which utilizes xkb
    keyboard configuration database [2].
    
    xkbcommon simplifies keyboard state handling by providing xkb_state
    object which holds the current state of all modifiers, groups, LEDs,
    etc, relating to the provided keymap.
    
    Detecting modifier mapping should become simpler once it gets supported
    in libxkbcommon. Also with xinput2 we could get rid of the XkbStateNotify
    events, because xinput2 key press/release already include all the mod/group
    info.
    
    [1] http://xkbcommon.org/
    [2] http://www.freedesktop.org/wiki/Software/XKeyboardConfig
    
    This patch:
    
    - Removes a dependency to -lxcb-keysyms
    - Removes a dependency to XLib call - XLookupString
    - Enables required functionality to fix QTBUG-27681
    
    Task-number: QTBUG-27680
    Change-Id: I10e10f873821ee02f6df72238e215a541150f38f
    Reviewed-by: Samuel Rødal <samuel.rodal@digia.com>
    
    cregit: 05351b0cde3e80d2d0ae3a838e802ff6086e4b59 https://github.com/qt/qtbase/commit/05351b0cde3e80d2d0ae3a838e802ff6086e4b59

--- src/plugins/platforms/xcb/qxcbkeyboard.h
+++ src/plugins/platforms/xcb/qxcbkeyboard.h
@@ -21,7 +21,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|"xcb/xcb_keysyms.h"
+file|<xkbcommon/xkbcommon.h>
 end_include
 begin_include
 include|#
@@ -85,7 +85,7 @@ name|void
 name|handleMappingNotifyEvent
 argument_list|(
 specifier|const
-name|xcb_mapping_notify_event_t
+name|xcb_xkb_map_notify_event_t
 operator|*
 name|event
 argument_list|)
@@ -97,8 +97,29 @@ name|translateModifiers
 argument_list|(
 argument|int s
 argument_list|)
+specifier|const
 block|;
-name|private
+name|void
+name|updateKeymap
+argument_list|()
+block|;
+name|void
+name|updateXKBState
+argument_list|(
+name|xcb_xkb_state_notify_event_t
+operator|*
+name|state
+argument_list|)
+block|;
+name|int
+name|coreDeviceId
+argument_list|()
+block|{
+return|return
+name|core_device_id
+return|;
+block|}
+name|protected
 operator|:
 name|void
 name|handleKeyEvent
@@ -114,82 +135,88 @@ argument_list|,
 argument|xcb_timestamp_t time
 argument_list|)
 block|;
-name|int
-name|translateKeySym
+name|QString
+name|keysymToUnicode
 argument_list|(
-argument|uint key
+argument|xcb_keysym_t sym
 argument_list|)
 specifier|const
 block|;
-name|QString
-name|translateKeySym
+name|int
+name|keysymToQtKey
+argument_list|(
+argument|xcb_keysym_t keysym
+argument_list|)
+specifier|const
+block|;
+name|int
+name|keysymToQtKey
 argument_list|(
 argument|xcb_keysym_t keysym
 argument_list|,
-argument|uint xmodifiers
-argument_list|,
-argument|int&code
-argument_list|,
 argument|Qt::KeyboardModifiers&modifiers
 argument_list|,
-argument|QByteArray&chars
-argument_list|,
-argument|int&count
+argument|QString text
+argument_list|)
+specifier|const
+block|;
+name|void
+name|readXKBConfig
+argument_list|(
+expr|struct
+name|xkb_rule_names
+operator|*
+name|names
 argument_list|)
 block|;
 name|void
-name|setupModifiers
+name|updateVModMapping
 argument_list|()
 block|;
 name|void
-name|setMask
-argument_list|(
-argument|uint sym
-argument_list|,
-argument|uint mask
-argument_list|)
+name|updateVModToRModMapping
+argument_list|()
 block|;
-name|xcb_keysym_t
-name|lookupString
-argument_list|(
-argument|QWindow *window
-argument_list|,
-argument|uint state
-argument_list|,
-argument|xcb_keycode_t code
-argument_list|,
-argument|QEvent::Type type
-argument_list|,
-argument|QByteArray *chars
-argument_list|)
-block|;
-name|uint
-name|m_alt_mask
-block|;
-name|uint
-name|m_super_mask
-block|;
-name|uint
-name|m_hyper_mask
-block|;
-name|uint
-name|m_meta_mask
-block|;
-name|uint
-name|m_mode_switch_mask
-block|;
-name|uint
-name|m_num_lock_mask
-block|;
-name|uint
-name|m_caps_lock_mask
-block|;
-name|xcb_key_symbols_t
-operator|*
-name|m_key_symbols
-block|;
+name|private
+operator|:
 name|xcb_keycode_t
 name|m_autorepeat_code
+block|;      struct
+name|xkb_context
+operator|*
+name|xkb_context
+block|;     struct
+name|xkb_keymap
+operator|*
+name|xkb_keymap
+block|;     struct
+name|xkb_state
+operator|*
+name|xkb_state
+block|;      struct
+name|_mod_masks
+block|{
+name|uint
+name|alt
+block|;
+name|uint
+name|altgr
+block|;
+name|uint
+name|meta
+block|;     }
+block|;
+name|_mod_masks
+name|vmod_masks
+block|;
+name|_mod_masks
+name|rmod_masks
+block|;
+name|int
+name|core_device_id
+block|;
+name|bool
+name|m_config
 block|; }
 decl_stmt|;
 end_decl_stmt
