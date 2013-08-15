3e7405f535300a17ff187bb24f770344f822e5c5
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@digia.com>
2013-08-15 10:45:51 +0200

    Android: Fix orientation change on Android 4.3
    In Android 4.3, we will get a new native window pointer for the
    same surface (the old surface has not been destroyed), whereas
    before we would get the same pointer, thus hitting the
    "sameNativeWindow" branch. This revealed some bugs in the
    surfaceChanged code path when the old surface had not been
    destroyed. This path is now taken both when there's no old surface
    (the app has been suspended in the mean time) and when the
    orientation changes.
    
    To handle the second case, we need to make sure:
    1. We update the static pointer
    2. We update the pointers in the platform windows
    3. We don't add a second reference to the static data for
    windows
    4. We schedule an update of the window size
    
    Task-number: QTBUG-32878
    Change-Id: I47257615f9ba820315fc98d7a804e52223f430bf
    Reviewed-by: Christian Stromme <christian.stromme@digia.com>
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@digia.com>
    
    cregit: 05ddae12d18a348afd942fd85a8d8773698da3a1 https://github.com/qt/qtbase/commit/05ddae12d18a348afd942fd85a8d8773698da3a1

--- src/plugins/platforms/android/src/qandroidplatformintegration.cpp
+++ src/plugins/platforms/android/src/qandroidplatformintegration.cpp
@@ -558,6 +558,11 @@ operator|::
 name|surfaceChanged
 parameter_list|()
 block|{
+name|QAndroidOpenGLPlatformWindow
+operator|::
+name|updateStaticNativeWindow
+argument_list|()
+expr_stmt|;
 foreach|foreach
 control|(
 name|QWindow
