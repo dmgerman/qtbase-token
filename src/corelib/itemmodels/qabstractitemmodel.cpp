a14ffb4276b58b7761086e79aa4589c2a51a24a4
Sze Howe Koh <szehowe.koh@gmail.com>
2013-09-08 18:57:09 +0800

    Doc: Fix copy+paste errors for QAbstractItemModel
    Change-Id: Ia462544cc86870d9870ae1be3b4f9135c9efe45c
    Reviewed-by: Stephen Kelly <stephen.kelly@kdab.com>
    
    cregit: 77056f91cd1dd9e88df04ff646c98213270b49b9 https://github.com/qt/qtbase/commit/77056f91cd1dd9e88df04ff646c98213270b49b9

--- src/corelib/itemmodels/qabstractitemmodel.cpp
+++ src/corelib/itemmodels/qabstractitemmodel.cpp
@@ -4211,7 +4211,7 @@ begin_comment
 comment|/*!     \fn bool QAbstractItemModel::removeRow(int row, const QModelIndex&parent)      Removes the given \a row from the child items of the \a parent specified.      Returns true if the row is removed; otherwise returns false.      This is a convenience function that calls removeRows(). The     QAbstractItemModel implementation of removeRows() does nothing.      \sa removeRows(), removeColumn(), insertRow() */
 end_comment
 begin_comment
-comment|/*!     \fn bool QAbstractItemModel::moveRow(const QModelIndex&sourceParent, int sourceColumn, const QModelIndex&destinationParent, int destinationChild)      On models that support this, moves \a sourceColumn from \a sourceParent to \a destinationChild under     \a destinationParent.      Returns true if the columns were successfully moved; otherwise returns     false.      \sa moveRows(), moveColumn() */
+comment|/*!     \fn bool QAbstractItemModel::moveRow(const QModelIndex&sourceParent, int sourceRow, const QModelIndex&destinationParent, int destinationChild)      On models that support this, moves \a sourceRow from \a sourceParent to \a destinationChild under     \a destinationParent.      Returns true if the rows were successfully moved; otherwise returns     false.      \sa moveRows(), moveColumn() */
 end_comment
 begin_comment
 comment|/*!     \fn bool QAbstractItemModel::moveColumn(const QModelIndex&sourceParent, int sourceColumn, const QModelIndex&destinationParent, int destinationChild)      On models that support this, moves \a sourceColumn from \a sourceParent to \a destinationChild under     \a destinationParent.      Returns true if the columns were successfully moved; otherwise returns     false.      \sa moveColumns(), moveRow() */
