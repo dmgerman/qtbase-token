20646a5a7c6710d93f083d28809fdb0ab509f1b7
Roger Maclean <rmaclean@qnx.com>
2013-10-10 15:25:55 -0400

    Add support for BB10 input method framework
    Added input method support for the BB10 variant of Qt to the extent
    possible using standard Qt APIs.  This adds support for text predictions
    and entry of languages such as Chinese.
    
    Change in interface to QQnxAbstractVirtualKeyboard was made because it
    is felt the new one is slightly nicer.  It doesn't appear safe to
    assume the focus object has a particular property and in fact in my tests
    the code failed to work.
    
    In some cases the code uses variable and function naming at odds with
    normal Qt coding standards.  This has been done for functions called
    and data provided by the BB10 input system as for those of us who
    need to maintain such things, it makes their meaning considerably
    clearer.
    
    While qqnxinputcontext_imf.cpp was used as an initial base for
    development one can consider the new version as largely new code. I
    don't believe the original version was ever complete and in any event
    would not compile.
    
    Change-Id: I09470801ffa237cee67da40c0b3d02ed5c77531e
    Reviewed-by: Rafael Roquetto <rafael.roquetto@kdab.com>
    
    cregit: db98b052ac671122437f181feeffb0b270318d8d https://github.com/qt/qtbase/commit/db98b052ac671122437f181feeffb0b270318d8d

--- src/plugins/platforms/qnx/qqnxintegration.cpp
+++ src/plugins/platforms/qnx/qqnxintegration.cpp
@@ -1,6 +1,6 @@
 begin_unit
 begin_comment
-comment|/*************************************************************************** ** ** Copyright (C) 2011 - 2012 Research In Motion ** Contact: http://www.qt-project.org/legal ** ** This file is part of the plugins of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** Commercial License Usage ** Licensees holding valid commercial Qt licenses may use this file in ** accordance with the commercial license agreement provided with the ** Software or, alternatively, in accordance with the terms contained in ** a written agreement between you and Digia.  For licensing terms and ** conditions see http://qt.digia.com/licensing.  For further information ** use the contact form at http://qt.digia.com/contact-us. ** ** GNU Lesser General Public License Usage ** Alternatively, this file may be used under the terms of the GNU Lesser ** General Public License version 2.1 as published by the Free Software ** Foundation and appearing in the file LICENSE.LGPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU Lesser General Public License version 2.1 requirements ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Digia gives you certain additional ** rights.  These rights are described in the Digia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU ** General Public License version 3.0 as published by the Free Software ** Foundation and appearing in the file LICENSE.GPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU General Public License version 3.0 requirements will be ** met: http://www.gnu.org/copyleft/gpl.html. ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
+comment|/*************************************************************************** ** ** Copyright (C) 2013 BlackBerry Limited. All rights reserved. ** Contact: http://www.qt-project.org/legal ** ** This file is part of the plugins of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** Commercial License Usage ** Licensees holding valid commercial Qt licenses may use this file in ** accordance with the commercial license agreement provided with the ** Software or, alternatively, in accordance with the terms contained in ** a written agreement between you and Digia.  For licensing terms and ** conditions see http://qt.digia.com/licensing.  For further information ** use the contact form at http://qt.digia.com/contact-us. ** ** GNU Lesser General Public License Usage ** Alternatively, this file may be used under the terms of the GNU Lesser ** General Public License version 2.1 as published by the Free Software ** Foundation and appearing in the file LICENSE.LGPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU Lesser General Public License version 2.1 requirements ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Digia gives you certain additional ** rights.  These rights are described in the Digia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU ** General Public License version 3.0 as published by the Free Software ** Foundation and appearing in the file LICENSE.GPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU General Public License version 3.0 requirements will be ** met: http://www.gnu.org/copyleft/gpl.html. ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
 end_comment
 begin_include
 include|#
@@ -880,6 +880,21 @@ operator|*
 name|m_virtualKeyboard
 argument_list|)
 expr_stmt|;
+if|#
+directive|if
+name|defined
+argument_list|(
+name|QQNX_IMF
+argument_list|)
+name|m_screenEventHandler
+operator|->
+name|addScreenEventFilter
+argument_list|(
+name|m_inputContext
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
 endif|#
 directive|endif
 block|}
@@ -939,26 +954,6 @@ name|m_drag
 expr_stmt|;
 endif|#
 directive|endif
-if|#
-directive|if
-name|defined
-argument_list|(
-name|QQNX_PPS
-argument_list|)
-comment|// Destroy the hardware button notifier
-operator|delete
-name|m_buttonsNotifier
-expr_stmt|;
-comment|// Destroy input context
-operator|delete
-name|m_inputContext
-expr_stmt|;
-endif|#
-directive|endif
-comment|// Destroy the keyboard class.
-operator|delete
-name|m_virtualKeyboard
-expr_stmt|;
 if|#
 directive|if
 operator|!
@@ -1061,6 +1056,26 @@ argument_list|()
 expr_stmt|;
 endif|#
 directive|endif
+if|#
+directive|if
+name|defined
+argument_list|(
+name|QQNX_PPS
+argument_list|)
+comment|// Destroy the hardware button notifier
+operator|delete
+name|m_buttonsNotifier
+expr_stmt|;
+comment|// Destroy input context
+operator|delete
+name|m_inputContext
+expr_stmt|;
+endif|#
+directive|endif
+comment|// Destroy the keyboard class.
+operator|delete
+name|m_virtualKeyboard
+expr_stmt|;
 comment|// Destroy services class
 operator|delete
 name|m_services
