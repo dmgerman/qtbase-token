240fd30f5011407edf3f86518b6db3a2afca1082
Maurice Kalinowski <maurice.kalinowski@digia.com>
2013-11-01 16:16:15 +0100

    Refactor plugin loading for WinRT
    Specify the root directory to be the package root. Only plugins
    inside the root can be opened (actually also only files). Furthermore
    current defaults to the package root now, which in most cases is
    identical to previous behavior.
    
    When attempting to load a plugin the path can either be specified in
    host format "C:/..." or as plugin absolute "/platforms/...". Check for
    both, with preference of latter case, like when qt.conf is used with
    / being used as plugin path.
    
    Change-Id: I7e3da293362488b62a3357c4882ebf5e048dcf95
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: Andrew Knight <andrew.knight@digia.com>
    Reviewed-by: Oliver Wolff <oliver.wolff@digia.com>
    
    cregit: 2ff0746e97acf37600b104838c96468f00953737 https://github.com/qt/qtbase/commit/2ff0746e97acf37600b104838c96468f00953737

--- src/corelib/io/qdir.cpp
+++ src/corelib/io/qdir.cpp
@@ -6725,6 +6725,12 @@ name|defined
 argument_list|(
 name|Q_OS_WINCE
 argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|Q_OS_WINRT
+argument_list|)
 comment|//allow unc paths
 name|allowUncPaths
 operator|=
