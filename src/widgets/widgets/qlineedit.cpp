0d08610ea9272e97d8caa5a5854d4696a5af2025
J-P Nurmi <jpnurmi@digia.com>
2013-12-02 14:42:58 +0100

    QLineEdit: hide placeholder text when h-centered & focused
    [ChangeLog][QtWidgets][QLineEdit] A blinking cursor in the middle
    over horizontally centered placeholder text looks bad. Thus,
    horizontally centered content is now considered as an exception
    and the placeholder text is hidden when the line edit is focused.
    
    Task-number: QTBUG-31669
    Change-Id: I17aa1e6656673f81545a8437f90814b188ad484a
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
    
    cregit: a2666d33919807ba6170f55f8e74a71ef8392a4d https://github.com/qt/qtbase/commit/a2666d33919807ba6170f55f8e74a71ef8392a4d

--- src/widgets/widgets/qlineedit.cpp
+++ src/widgets/widgets/qlineedit.cpp
@@ -538,7 +538,7 @@ expr_stmt|;
 block|}
 end_function
 begin_comment
-comment|/*!     \since 4.7      \property QLineEdit::placeholderText     \brief the line edit's placeholder text      Setting this property makes the line edit display a grayed-out     placeholder text as long as the text() is empty.      By default, this property contains an empty string.      \sa text() */
+comment|/*!     \since 4.7      \property QLineEdit::placeholderText     \brief the line edit's placeholder text      Setting this property makes the line edit display a grayed-out     placeholder text as long as the line edit is empty.      Normally, an empty line edit shows the placeholder text even     when it has focus. However, if the content is horizontally     centered, the placeholder text is not displayed under     the cursor when the line edit has focus.      By default, this property contains an empty string.      \sa text() */
 end_comment
 begin_function
 DECL|function|placeholderText
