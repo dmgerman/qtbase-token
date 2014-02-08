b3945305fb36208ce11801bb2140f576bd149f6f
Sérgio Martins <sergio.martins@kdab.com>
2014-02-08 00:11:56 +0000

    Fix build with QT_NO_GRAPHICSVIEW
    Change-Id: I81cfed124a2e9e1c46c966c4819e89df5abd4f74
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
    
    cregit: 63cf8ad33a4671aee35ac995bd34e83b1d863ac4 https://github.com/qt/qtbase/commit/63cf8ad33a4671aee35ac995bd34e83b1d863ac4

--- src/widgets/graphicsview/qgraphicsgridlayoutengine_p.h
+++ src/widgets/graphicsview/qgraphicsgridlayoutengine_p.h
@@ -18,6 +18,11 @@ include|#
 directive|include
 file|<QtGui/private/qgridlayoutengine_p.h>
 end_include
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|QT_NO_GRAPHICSVIEW
+end_ifndef
 begin_include
 include|#
 directive|include
@@ -417,6 +422,13 @@ begin_macro
 unit|};
 name|QT_END_NAMESPACE
 end_macro
+begin_endif
+endif|#
+directive|endif
+end_endif
+begin_comment
+comment|// QT_NO_GRAPHICSVIEW
+end_comment
 begin_endif
 endif|#
 directive|endif
