6d22f795c00108e09ec6b1f6cf99cbfd08bb9246
Morten Johan Sørvig <morten.sorvig@digia.com>
2014-03-13 12:46:10 +0100

    Cocoa: Disable the zoom button when appropriate
    Disable the zoom (maximize) button for fixed-size
    windows and customized windows with the MaximizeButton
    flag not set.
    
    Task-number: QTBUG-37078
    Change-Id: I6da88496474713de37b48aa65742203632ba99d6
    Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>
    
    cregit: 4d56c86f802a49e0fa5481e13ab1f000ff284637 https://github.com/qt/qtbase/commit/4d56c86f802a49e0fa5481e13ab1f000ff284637

--- src/plugins/platforms/cocoa/qcocoawindow.h
+++ src/plugins/platforms/cocoa/qcocoawindow.h
@@ -637,6 +637,12 @@ argument|Qt::WindowFlags flags
 argument_list|)
 block|;
 name|void
+name|setWindowZoomButton
+argument_list|(
+argument|Qt::WindowFlags flags
+argument_list|)
+block|;
+name|void
 name|setCurrentContext
 argument_list|(
 name|QCocoaGLContext
