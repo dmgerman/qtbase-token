19fed3107a8038ec7b2215bdc8e702277f52961f
Laszlo Agocs <laszlo.agocs@digia.com>
2014-04-24 17:13:40 +0200

    Remove old symbols from QOpenGLContext
    Task-number: QTBUG-38564
    Change-Id: I39a0482e4411cdc644ed34813626c39dacb5b6af
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    
    cregit: 8ddf76989c80896d4cd1e759bbd2594609eebf6d https://github.com/qt/qtbase/commit/8ddf76989c80896d4cd1e759bbd2594609eebf6d

--- src/gui/kernel/qopenglcontext.h
+++ src/gui/kernel/qopenglcontext.h
@@ -665,15 +665,6 @@ block|{
 name|LibGL
 block|,
 name|LibGLES
-block|,
-comment|// ###
-name|DesktopGL
-operator|=
-name|LibGL
-block|,
-name|GLES2
-operator|=
-name|LibGLES
 block|}
 block|;
 specifier|static
@@ -686,17 +677,6 @@ name|isOpenGLES
 argument_list|()
 specifier|const
 block|;
-name|bool
-name|isES
-argument_list|()
-specifier|const
-block|{
-return|return
-name|isOpenGLES
-argument_list|()
-return|;
-block|}
-comment|// ###
 name|Q_SIGNALS
 operator|:
 name|void
