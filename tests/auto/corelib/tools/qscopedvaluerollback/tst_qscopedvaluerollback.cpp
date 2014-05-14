1e2346b665daa5529860b35d25d17399938ba9c7
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2014-05-14 10:52:37 +0200

    qscopedvaluerollback: add convenience constructor
    It's a common need to assign a variable to something when entering a
    code block, and then revert it upon exit. qscopedvaluerollback can
    be used for this. But as a convenience, this patch adds an
    extra constructor so that you can "protect" and set a variable
    in one go instead of using two lines.
    
    Change-Id: If4b89d3a5ba32ef2304bda058b1b6050932612ed
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 77982c482323116c59949147291fe4f8ac901f5d https://github.com/qt/qtbase/commit/77982c482323116c59949147291fe4f8ac901f5d

--- tests/auto/corelib/tools/qscopedvaluerollback/tst_qscopedvaluerollback.cpp
+++ tests/auto/corelib/tools/qscopedvaluerollback/tst_qscopedvaluerollback.cpp
@@ -1,6 +1,6 @@
 begin_unit
 begin_comment
-comment|/**************************************************************************** ** ** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies). ** Contact: http://www.qt-project.org/legal ** ** This file is part of the test suite of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** Commercial License Usage ** Licensees holding valid commercial Qt licenses may use this file in ** accordance with the commercial license agreement provided with the ** Software or, alternatively, in accordance with the terms contained in ** a written agreement between you and Digia.  For licensing terms and ** conditions see http://qt.digia.com/licensing.  For further information ** use the contact form at http://qt.digia.com/contact-us. ** ** GNU Lesser General Public License Usage ** Alternatively, this file may be used under the terms of the GNU Lesser ** General Public License version 2.1 as published by the Free Software ** Foundation and appearing in the file LICENSE.LGPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU Lesser General Public License version 2.1 requirements ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Digia gives you certain additional ** rights.  These rights are described in the Digia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU ** General Public License version 3.0 as published by the Free Software ** Foundation and appearing in the file LICENSE.GPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU General Public License version 3.0 requirements will be ** met: http://www.gnu.org/copyleft/gpl.html. ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
+comment|/**************************************************************************** ** ** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies). ** Contact: http://www.qt-project.org/legal ** ** This file is part of the test suite of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** Commercial License Usage ** Licensees holding valid commercial Qt licenses may use this file in ** accordance with the commercial license agreement provided with the ** Software or, alternatively, in accordance with the terms contained in ** a written agreement between you and Digia.  For licensing terms and ** conditions see http://qt.digia.com/licensing.  For further information ** use the contact form at http://qt.digia.com/contact-us. ** ** GNU Lesser General Public License Usage ** Alternatively, this file may be used under the terms of the GNU Lesser ** General Public License version 2.1 as published by the Free Software ** Foundation and appearing in the file LICENSE.LGPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU Lesser General Public License version 2.1 requirements ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Digia gives you certain additional ** rights.  These rights are described in the Digia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU ** General Public License version 3.0 as published by the Free Software ** Foundation and appearing in the file LICENSE.GPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU General Public License version 3.0 requirements will be ** met: http://www.gnu.org/copyleft/gpl.html. ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
 end_comment
 begin_include
 include|#
@@ -80,6 +80,11 @@ name|b
 init|=
 literal|false
 decl_stmt|;
+name|bool
+name|b2
+init|=
+literal|false
+decl_stmt|;
 name|QString
 name|s
 argument_list|(
@@ -108,6 +113,17 @@ argument_list|)
 decl_stmt|;
 name|QScopedValueRollback
 argument_list|<
+name|bool
+argument_list|>
+name|rb2
+argument_list|(
+name|b2
+argument_list|,
+literal|true
+argument_list|)
+decl_stmt|;
+name|QScopedValueRollback
+argument_list|<
 name|QString
 argument_list|>
 name|rs
@@ -120,6 +136,13 @@ argument_list|(
 name|b
 argument_list|,
 literal|false
+argument_list|)
+expr_stmt|;
+name|QCOMPARE
+argument_list|(
+name|b2
+argument_list|,
+literal|true
 argument_list|)
 expr_stmt|;
 name|QCOMPARE
@@ -179,6 +202,13 @@ block|}
 name|QCOMPARE
 argument_list|(
 name|b
+argument_list|,
+literal|false
+argument_list|)
+expr_stmt|;
+name|QCOMPARE
+argument_list|(
+name|b2
 argument_list|,
 literal|false
 argument_list|)
