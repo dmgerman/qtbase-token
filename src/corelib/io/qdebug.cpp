ef18d45a93b46e3324c5c0d31f577cdec9e75eb8
Kai Koehne <kai.koehne@digia.com>
2014-06-20 14:44:48 +0200

    QDebug: Add resetFormat()
    Similar to QTextStream::reset(), this resets the stream format to the
    defaults. Its primary use is inside custom operator<< implementations,
    where you'd want to have a fixed format regardless of the current
    stream state.
    
    Change-Id: I421d76c61f164579bb90cf4195cc5376e2dcf0f3
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
    
    cregit: 87aceebf42af5bbc7ea96ac1a60a7ba030ad3e3f https://github.com/qt/qtbase/commit/87aceebf42af5bbc7ea96ac1a60a7ba030ad3e3f

--- src/corelib/io/qdebug.cpp
+++ src/corelib/io/qdebug.cpp
@@ -1,6 +1,6 @@
 begin_unit
 begin_comment
-comment|/**************************************************************************** ** ** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies). ** Contact: http://www.qt-project.org/legal ** ** This file is part of the QtCore module of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** Commercial License Usage ** Licensees holding valid commercial Qt licenses may use this file in ** accordance with the commercial license agreement provided with the ** Software or, alternatively, in accordance with the terms contained in ** a written agreement between you and Digia.  For licensing terms and ** conditions see http://qt.digia.com/licensing.  For further information ** use the contact form at http://qt.digia.com/contact-us. ** ** GNU Lesser General Public License Usage ** Alternatively, this file may be used under the terms of the GNU Lesser ** General Public License version 2.1 as published by the Free Software ** Foundation and appearing in the file LICENSE.LGPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU Lesser General Public License version 2.1 requirements ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Digia gives you certain additional ** rights.  These rights are described in the Digia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU ** General Public License version 3.0 as published by the Free Software ** Foundation and appearing in the file LICENSE.GPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU General Public License version 3.0 requirements will be ** met: http://www.gnu.org/copyleft/gpl.html. ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
+comment|/**************************************************************************** ** ** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies). ** Contact: http://www.qt-project.org/legal ** ** This file is part of the QtCore module of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** Commercial License Usage ** Licensees holding valid commercial Qt licenses may use this file in ** accordance with the commercial license agreement provided with the ** Software or, alternatively, in accordance with the terms contained in ** a written agreement between you and Digia.  For licensing terms and ** conditions see http://qt.digia.com/licensing.  For further information ** use the contact form at http://qt.digia.com/contact-us. ** ** GNU Lesser General Public License Usage ** Alternatively, this file may be used under the terms of the GNU Lesser ** General Public License version 2.1 as published by the Free Software ** Foundation and appearing in the file LICENSE.LGPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU Lesser General Public License version 2.1 requirements ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Digia gives you certain additional ** rights.  These rights are described in the Digia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU ** General Public License version 3.0 as published by the Free Software ** Foundation and appearing in the file LICENSE.GPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU General Public License version 3.0 requirements will be ** met: http://www.gnu.org/copyleft/gpl.html. ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
 end_comment
 begin_ifdef
 ifdef|#
@@ -128,6 +128,53 @@ begin_comment
 unit|} }
 comment|/*!     \fn QDebug::swap(QDebug&other)     \since 5.0      Swaps this debug stream instance with \a other. This function is     very fast and never fails. */
 end_comment
+begin_comment
+comment|/*!     Resets the stream formatting options, bringing it back to its original constructed state.      \sa space(), quote()     \since 5.4 */
+end_comment
+begin_function
+DECL|function|resetFormat
+name|QDebug
+modifier|&
+name|QDebug
+operator|::
+name|resetFormat
+parameter_list|()
+block|{
+name|stream
+operator|->
+name|ts
+operator|.
+name|reset
+argument_list|()
+expr_stmt|;
+name|stream
+operator|->
+name|space
+operator|=
+literal|true
+expr_stmt|;
+if|if
+condition|(
+name|stream
+operator|->
+name|context
+operator|.
+name|version
+operator|>
+literal|1
+condition|)
+name|stream
+operator|->
+name|flags
+operator|=
+literal|0
+expr_stmt|;
+return|return
+operator|*
+name|this
+return|;
+block|}
+end_function
 begin_comment
 comment|/*!     \fn QDebug&QDebug::space()      Writes a space character to the debug stream and returns a reference to     the stream.      The stream remembers that automatic insertion of spaces is     enabled for future writes.      \sa nospace(), maybeSpace() */
 end_comment
