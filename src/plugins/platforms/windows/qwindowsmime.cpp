3674f8d6afafed356345512aea8abec46b4d7c5f
Friedemann Kleint <Friedemann.Kleint@digia.com>
2014-07-01 12:21:03 +0200

    Windows clipboard: Deny CF_DIB support for images with alpha.
    Support CF_DIBV5 for these images so that transparency is preserved.
    
    Task-number: QTBUG-11463
    Change-Id: I51881ae8bfbd05b92abd309766f4da9a7ea26c2d
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
    
    cregit: 9150563940f61be9fb17510be49c0734244866a5 https://github.com/qt/qtbase/commit/9150563940f61be9fb17510be49c0734244866a5

--- src/plugins/platforms/windows/qwindowsmime.cpp
+++ src/plugins/platforms/windows/qwindowsmime.cpp
@@ -1,6 +1,6 @@
 begin_unit
 begin_comment
-comment|/**************************************************************************** ** ** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies). ** Contact: http://www.qt-project.org/legal ** ** This file is part of the plugins of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** Commercial License Usage ** Licensees holding valid commercial Qt licenses may use this file in ** accordance with the commercial license agreement provided with the ** Software or, alternatively, in accordance with the terms contained in ** a written agreement between you and Digia.  For licensing terms and ** conditions see http://qt.digia.com/licensing.  For further information ** use the contact form at http://qt.digia.com/contact-us. ** ** GNU Lesser General Public License Usage ** Alternatively, this file may be used under the terms of the GNU Lesser ** General Public License version 2.1 as published by the Free Software ** Foundation and appearing in the file LICENSE.LGPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU Lesser General Public License version 2.1 requirements ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Digia gives you certain additional ** rights.  These rights are described in the Digia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU ** General Public License version 3.0 as published by the Free Software ** Foundation and appearing in the file LICENSE.GPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU General Public License version 3.0 requirements will be ** met: http://www.gnu.org/copyleft/gpl.html. ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
+comment|/**************************************************************************** ** ** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies). ** Contact: http://www.qt-project.org/legal ** ** This file is part of the plugins of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:LGPL$ ** Commercial License Usage ** Licensees holding valid commercial Qt licenses may use this file in ** accordance with the commercial license agreement provided with the ** Software or, alternatively, in accordance with the terms contained in ** a written agreement between you and Digia.  For licensing terms and ** conditions see http://qt.digia.com/licensing.  For further information ** use the contact form at http://qt.digia.com/contact-us. ** ** GNU Lesser General Public License Usage ** Alternatively, this file may be used under the terms of the GNU Lesser ** General Public License version 2.1 as published by the Free Software ** Foundation and appearing in the file LICENSE.LGPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU Lesser General Public License version 2.1 requirements ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html. ** ** In addition, as a special exception, Digia gives you certain additional ** rights.  These rights are described in the Digia Qt LGPL Exception ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package. ** ** GNU General Public License Usage ** Alternatively, this file may be used under the terms of the GNU ** General Public License version 3.0 as published by the Free Software ** Foundation and appearing in the file LICENSE.GPL included in the ** packaging of this file.  Please review the following information to ** ensure the GNU General Public License version 3.0 requirements will be ** met: http://www.gnu.org/copyleft/gpl.html. ** ** ** $QT_END_LICENSE$ ** ****************************************************************************/
 end_comment
 begin_include
 include|#
@@ -5709,30 +5709,16 @@ argument_list|)
 decl_stmt|;
 if|if
 condition|(
+operator|!
 name|mimeData
 operator|->
 name|hasImage
 argument_list|()
 condition|)
-block|{
-if|if
-condition|(
-name|cf
-operator|==
-name|CF_DIB
-condition|)
 return|return
-literal|true
+literal|false
 return|;
-elseif|else
-if|if
-condition|(
-name|cf
-operator|==
-name|CF_DIBV5
-condition|)
-block|{
-comment|//support DIBV5 conversion only if the image has alpha channel
+specifier|const
 name|QImage
 name|image
 init|=
@@ -5749,24 +5735,32 @@ argument_list|)
 decl_stmt|;
 if|if
 condition|(
-operator|!
 name|image
 operator|.
 name|isNull
 argument_list|()
+condition|)
+return|return
+literal|false
+return|;
+comment|// QTBUG-11463, deny CF_DIB support for images with alpha to prevent loss of
+comment|// transparency in conversion.
+return|return
+name|cf
+operator|==
+name|CF_DIBV5
+operator|||
+operator|(
+name|cf
+operator|==
+name|CF_DIB
 operator|&&
+operator|!
 name|image
 operator|.
 name|hasAlphaChannel
 argument_list|()
-condition|)
-return|return
-literal|true
-return|;
-block|}
-block|}
-return|return
-literal|false
+operator|)
 return|;
 block|}
 end_function
