d2c76a26120351968bf763ddd614cc9370dde22d
Kai Koehne <kai.koehne@digia.com>
2014-07-11 12:10:42 +0200

    Improve QLoggingCategory example code in documentation
    Don't use a qt.* category in the user documentation: These should be
    reserved to Qt internal use, and also have special semantics (QtDebugMsg
    by default off) that might lead to confusion.
    
    Also, adapt the camel case naming convention for logging categories
    defined by the macros. This was the (although somewhat controversial)
    result of a recent discussion on the development mailing list.
    
    Change-Id: Ic7162b47bb2d76550c766bc40dd65ce039e7e3eb
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: Alex Blasche <alexander.blasche@digia.com>
    
    cregit: 0fe2713e260c8ecfa463a7075a6b9f1892000691 https://github.com/qt/qtbase/commit/0fe2713e260c8ecfa463a7075a6b9f1892000691

--- src/corelib/doc/snippets/qloggingcategory/main.cpp
+++ src/corelib/doc/snippets/qloggingcategory/main.cpp
@@ -1,6 +1,6 @@
 begin_unit
 begin_comment
-comment|/**************************************************************************** ** ** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies). ** Contact: http://www.qt-project.org/legal ** ** This file is part of the examples of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:BSD$ ** You may use this file under the terms of the BSD license as follows: ** ** "Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions are ** met: **   * Redistributions of source code must retain the above copyright **     notice, this list of conditions and the following disclaimer. **   * Redistributions in binary form must reproduce the above copyright **     notice, this list of conditions and the following disclaimer in **     the documentation and/or other materials provided with the **     distribution. **   * Neither the name of Digia Plc and its Subsidiary(-ies) nor the names **     of its contributors may be used to endorse or promote products derived **     from this software without specific prior written permission. ** ** ** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT ** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR ** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT ** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, ** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT ** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE ** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." ** ** $QT_END_LICENSE$ ** ****************************************************************************/
+comment|/**************************************************************************** ** ** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies). ** Contact: http://www.qt-project.org/legal ** ** This file is part of the examples of the Qt Toolkit. ** ** $QT_BEGIN_LICENSE:BSD$ ** You may use this file under the terms of the BSD license as follows: ** ** "Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions are ** met: **   * Redistributions of source code must retain the above copyright **     notice, this list of conditions and the following disclaimer. **   * Redistributions in binary form must reproduce the above copyright **     notice, this list of conditions and the following disclaimer in **     the documentation and/or other materials provided with the **     distribution. **   * Neither the name of Digia Plc and its Subsidiary(-ies) nor the names **     of its contributors may be used to endorse or promote products derived **     from this software without specific prior written permission. ** ** ** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT ** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR ** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT ** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, ** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT ** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE ** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." ** ** $QT_END_LICENSE$ ** ****************************************************************************/
 end_comment
 begin_include
 include|#
@@ -21,7 +21,7 @@ end_comment
 begin_macro
 name|Q_DECLARE_LOGGING_CATEGORY
 argument_list|(
-argument|QT_DRIVER_USB
+argument|driverUsb
 argument_list|)
 end_macro
 begin_comment
@@ -30,9 +30,9 @@ end_comment
 begin_macro
 name|Q_LOGGING_CATEGORY
 argument_list|(
-argument|QT_DRIVER_USB
+argument|driverUsb
 argument_list|,
-literal|"qt.driver.usb"
+literal|"driver.usb"
 argument_list|)
 end_macro
 begin_comment
@@ -156,7 +156,7 @@ modifier|*
 name|category
 parameter_list|)
 block|{
-comment|// configure qt.driver.usb category here, otherwise forward to to default filter.
+comment|// configure driver.usb category here, otherwise forward to to default filter.
 if|if
 condition|(
 name|qstrcmp
@@ -166,7 +166,7 @@ operator|->
 name|categoryName
 argument_list|()
 argument_list|,
-literal|"qt.driver.usb"
+literal|"driver.usb"
 argument_list|)
 operator|==
 literal|0
@@ -220,7 +220,7 @@ name|setFilterRules
 argument_list|(
 name|QStringLiteral
 argument_list|(
-literal|"qt.driver.usb.debug=true"
+literal|"driver.usb.debug=true"
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -245,10 +245,10 @@ argument_list|)
 expr_stmt|;
 comment|//![3]
 comment|//![4]
-comment|// usbEntries() will only be called if QT_DRIVER_USB category is enabled
+comment|// usbEntries() will only be called if driverUsb category is enabled
 name|qCDebug
 argument_list|(
-name|QT_DRIVER_USB
+name|driverUsb
 argument_list|)
 operator|<<
 literal|"devices: "
@@ -262,7 +262,7 @@ comment|//![10]
 name|QLoggingCategory
 name|category
 argument_list|(
-literal|"qt.driver.usb"
+literal|"driver.usb"
 argument_list|)
 decl_stmt|;
 name|qCDebug
@@ -279,7 +279,7 @@ comment|//![11]
 name|QLoggingCategory
 name|category
 argument_list|(
-literal|"qt.driver.usb"
+literal|"driver.usb"
 argument_list|)
 decl_stmt|;
 name|qCWarning
@@ -296,7 +296,7 @@ comment|//![12]
 name|QLoggingCategory
 name|category
 argument_list|(
-literal|"qt.driver.usb"
+literal|"driver.usb"
 argument_list|)
 decl_stmt|;
 name|qCCritical
@@ -313,7 +313,7 @@ comment|//![13]
 name|QLoggingCategory
 name|category
 argument_list|(
-literal|"qt.driver.usb"
+literal|"driver.usb"
 argument_list|)
 decl_stmt|;
 name|qCDebug
@@ -335,7 +335,7 @@ comment|//![14]
 name|QLoggingCategory
 name|category
 argument_list|(
-literal|"qt.driver.usb"
+literal|"driver.usb"
 argument_list|)
 decl_stmt|;
 name|qCWarning
@@ -357,7 +357,7 @@ comment|//![15]
 name|QLoggingCategory
 name|category
 argument_list|(
-literal|"qt.driver.usb"
+literal|"driver.usb"
 argument_list|)
 decl_stmt|;
 name|qCCritical
