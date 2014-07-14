b44435c4b8b1ecaae39b800c1fc67b7c5e8866a3
Andrew Knight <andrew.knight@digia.com>
2014-07-15 02:02:58 +0300

    winrt: Fix orientation update mask
    On WP8.0, setting auto rotation preferences at runtime worked like an
    orientation update mask, and did not modify the behavior of the window.
    With WP8.1, setting auto rotation preferences has the side effect that the
    compositor will then resize the window when moving from e.g. portrait to
    landscape. Because of this, the auto rotation API should not be called
    inside orientationUpdateMask(). The default implementation is now
    sufficient, so the platform override is removed.
    
    Developers looking to set auto rotation preferences should use the
    application manifest or call the native api directly.
    
    Task-number: QTBUG-35953
    Change-Id: I90cf4290ced34df1bb350cb6aa5deff209622865
    Reviewed-by: Oliver Wolff <oliver.wolff@digia.com>
    
    cregit: c48484ea6d39909b7919577701ffeb9656bbea62 https://github.com/qt/qtbase/commit/c48484ea6d39909b7919577701ffeb9656bbea62

--- src/plugins/platforms/winrt/qwinrtscreen.h
+++ src/plugins/platforms/winrt/qwinrtscreen.h
@@ -231,12 +231,6 @@ name|orientation
 argument_list|()
 specifier|const
 block|;
-name|void
-name|setOrientationUpdateMask
-argument_list|(
-argument|Qt::ScreenOrientations mask
-argument_list|)
-block|;
 name|QWindow
 operator|*
 name|topWindow
