69eb6d0ec193ce1ce951151040ce7d232704f68c
Tasuku Suzuki <stasuku@gmail.com>
2014-08-25 22:46:38 +0900

    fix a camel case include guard macro
    Change-Id: I502ecf6c862f101e426536e11f5c466ed3419946
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 44b9e31a0a003319dc7946f157c890cb3285d55e https://github.com/qt/qtbase/commit/44b9e31a0a003319dc7946f157c890cb3285d55e

--- src/widgets/widgets/qwidgetlinecontrol_p.h
+++ src/widgets/widgets/qwidgetlinecontrol_p.h
@@ -5,13 +5,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|QWidgetLineControl_P_H
+name|QWIDGETLINECONTROL_P_H
 end_ifndef
 begin_define
-DECL|macro|QWidgetLineControl_P_H
+DECL|macro|QWIDGETLINECONTROL_P_H
 define|#
 directive|define
-name|QWidgetLineControl_P_H
+name|QWIDGETLINECONTROL_P_H
 end_define
 begin_comment
 comment|//
@@ -3211,6 +3211,6 @@ endif|#
 directive|endif
 end_endif
 begin_comment
-comment|// QWidgetLineControl_P_H
+comment|// QWIDGETLINECONTROL_P_H
 end_comment
 end_unit
