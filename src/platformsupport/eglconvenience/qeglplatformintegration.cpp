7dbef7c282086c113c110e42baf89337967b0907
Laszlo Agocs <laszlo.agocs@digia.com>
2014-10-03 14:57:29 +0200

    Rename internal enum value to avoid clashing with Xlib
    Change-Id: I4926d1f75ece220d28abf9f2e7081e41f0aaf339
    Reviewed-by: Jørgen Lind <jorgen.lind@digia.com>
    Reviewed-by: Martin Jansa <Martin.Jansa@gmail.com>
    
    cregit: 848e95025297f8f56443ea7b22a1ddca1cc63cd8 https://github.com/qt/qtbase/commit/848e95025297f8f56443ea7b22a1ddca1cc63cd8

--- src/platformsupport/eglconvenience/qeglplatformintegration.cpp
+++ src/platformsupport/eglconvenience/qeglplatformintegration.cpp
@@ -668,8 +668,8 @@ block|,
 DECL|enumerator|NativeDisplay
 name|NativeDisplay
 block|,
-DECL|enumerator|Display
-name|Display
+DECL|enumerator|XlibDisplay
+name|XlibDisplay
 block|}
 enum|;
 end_enum
@@ -887,7 +887,7 @@ argument_list|)
 condition|)
 block|{
 case|case
-name|Display
+name|XlibDisplay
 case|:
 comment|// Play nice when using the x11 hooks: Be compatible with xcb that allows querying
 comment|// the X Display pointer, which is nothing but our native display.
