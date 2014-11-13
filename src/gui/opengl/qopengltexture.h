cbf1d11d8ea06a1f0034f24ae4052056ae0578c8
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2014-11-13 12:54:35 +0100

    QOpenGLTexture: introduce support for Ericsson compressed formats
    OpenGL 4.3 and OpenGL ES 3.0 have support for these formats
    out of the box.
    
    See http://en.wikipedia.org/wiki/Ericsson_Texture_Compression
    
    Change-Id: I661f05b9872f3aa128e39cec64df094a79736555
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    
    cregit: c0b4e9076b7d62b1fbb1ea8c5983cf3814e0e2dd https://github.com/qt/qtbase/commit/c0b4e9076b7d62b1fbb1ea8c5983cf3814e0e2dd

--- src/gui/opengl/qopengltexture.h
+++ src/gui/opengl/qopengltexture.h
@@ -573,6 +573,56 @@ init|=
 literal|0x8E8C
 block|,
 comment|// GL_COMPRESSED_RGBA_BPTC_UNORM_ARB
+name|R11_EAC_UNorm
+init|=
+literal|0x9270
+block|,
+comment|// GL_COMPRESSED_R11_EAC
+name|R11_EAC_SNorm
+init|=
+literal|0x9271
+block|,
+comment|// GL_COMPRESSED_SIGNED_R11_EAC
+name|RG11_EAC_UNorm
+init|=
+literal|0x9272
+block|,
+comment|// GL_COMPRESSED_RG11_EAC
+name|RG11_EAC_SNorm
+init|=
+literal|0x9273
+block|,
+comment|// GL_COMPRESSED_SIGNED_RG11_EAC
+name|RGB8_ETC2
+init|=
+literal|0x9274
+block|,
+comment|// GL_COMPRESSED_RGB8_ETC2
+name|SRGB8_ETC2
+init|=
+literal|0x9275
+block|,
+comment|// GL_COMPRESSED_SRGB8_ETC2
+name|RGB8_PunchThrough_Alpha1_ETC2
+init|=
+literal|0x9276
+block|,
+comment|// GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2
+name|SRGB8_PunchThrough_Alpha1_ETC2
+init|=
+literal|0x9277
+block|,
+comment|// GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2
+name|RGBA8_ETC2_EAC
+init|=
+literal|0x9278
+block|,
+comment|// GL_COMPRESSED_RGBA8_ETC2_EAC
+name|SRGB8_Alpha8_ETC2_EAC
+init|=
+literal|0x9279
+block|,
+comment|// GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC
 comment|// sRGB formats
 name|SRGB8
 init|=
