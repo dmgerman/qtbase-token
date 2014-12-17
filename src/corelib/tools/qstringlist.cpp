f80958a35c57ac3cd1295faf5aabb5dd1ee12c1b
Thiago Macieira <thiago.macieira@intel.com>
2014-12-16 17:21:48 -0800

    Remove the operator<< | >> (QDataStream, QStringList)
    They aren't needed. The template version for QList in qdatastream.h is
    sufficient.
    
    This is binary compatible, since the functions were never exported. It's
    also source compatible, provided no one tried to do:
    
        QDataStream & (*fptr)(QDataStream &, QStringList &) = &operator>>;
    
    Change-Id: I8a4449b416154b34c007ef6abacf2b112451028f
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    
    cregit: 3c9847a068dc86e44b7a2dcbae80cdab308ffa4a https://github.com/qt/qtbase/commit/3c9847a068dc86e44b7a2dcbae80cdab308ffa4a

--- src/corelib/tools/qstringlist.cpp
+++ src/corelib/tools/qstringlist.cpp
@@ -887,24 +887,6 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|QT_NO_DATASTREAM
-end_ifndef
-begin_comment
-comment|/*!     \fn QDataStream&operator>>(QDataStream&in, QStringList&list)     \relates QStringList      Reads a string list from the given \a in stream into the specified     \a list.      \sa {Serializing Qt Data Types} */
-end_comment
-begin_comment
-comment|/*!     \fn QDataStream&operator<<(QDataStream&out, const QStringList&list)     \relates QStringList      Writes the given string \a list to the specified \a out stream.      \sa {Serializing Qt Data Types} */
-end_comment
-begin_endif
-endif|#
-directive|endif
-end_endif
-begin_comment
-comment|// QT_NO_DATASTREAM
-end_comment
-begin_ifndef
-ifndef|#
-directive|ifndef
 name|QT_NO_REGEXP
 end_ifndef
 begin_function
