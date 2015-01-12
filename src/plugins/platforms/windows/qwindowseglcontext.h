2389a6f3f1993f407879d7eafbbdf55d55e2b11f
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-01-12 15:59:36 +0100

    Windows: Add infrastructure to be able to a GL renderer based on GPU.
    Introduce flags for the renderer type and move code to
    qwindowsopengltester. Introduce
    QWindowsOpenGLTester::supportedGlesRenderers() where type-dependent
    checking can be added.
    
    Change-Id: I4bbffaf861cb0fdbea0919e081e3626fb5a872de
    Task-number: QTBUG-43263
    Reviewed-by: Laszlo Agocs <laszlo.agocs@theqtcompany.com>
    
    cregit: 97a82f62c46c272fe3ef1cd9d5c2214b6a7626af https://github.com/qt/qtbase/commit/97a82f62c46c272fe3ef1cd9d5c2214b6a7626af

--- src/plugins/platforms/windows/qwindowseglcontext.h
+++ src/plugins/platforms/windows/qwindowseglcontext.h
@@ -21,6 +21,11 @@ end_include
 begin_include
 include|#
 directive|include
+file|"qwindowsopengltester.h"
+end_include
+begin_include
+include|#
+directive|include
 file|<EGL/egl.h>
 end_include
 begin_macro
@@ -3059,7 +3064,9 @@ specifier|static
 name|QWindowsEGLStaticContext
 operator|*
 name|create
-argument_list|()
+argument_list|(
+argument|QWindowsOpenGLTester::Renderers preferredType
+argument_list|)
 block|;
 operator|~
 name|QWindowsEGLStaticContext
