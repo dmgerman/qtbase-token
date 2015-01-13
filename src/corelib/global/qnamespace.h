ee4306cdf717ada8bfbd0e8c94011ba64aa93bfa
Shawn Rutledge <shawn.rutledge@digia.com>
2015-01-13 15:54:15 +0100

    QEvent types: use Q_ENUM qDebug support instead of custom formatters
    Change-Id: I10886de57b3ba24dddfcd4b78e1a32c470ac1889
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
    
    cregit: 3b6d2575ae2662ae25b343adeb1f105ce8fd96d7 https://github.com/qt/qtbase/commit/3b6d2575ae2662ae25b343adeb1f105ce8fd96d7

--- src/corelib/global/qnamespace.h
+++ src/corelib/global/qnamespace.h
@@ -5168,6 +5168,10 @@ name|QT_Q_FLAG
 argument_list|(
 argument|InputMethodQueries
 argument_list|)
+name|QT_Q_FLAG
+argument_list|(
+argument|TouchPointStates
+argument_list|)
 name|QT_Q_ENUM
 argument_list|(
 argument|ScreenOrientation
