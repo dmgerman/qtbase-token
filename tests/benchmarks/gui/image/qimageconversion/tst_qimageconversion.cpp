72d318f5c631c81047c14d529817f6baa593072f
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2015-01-27 14:08:17 +0100

    Only use 32bit version of qPremultiply
    With auto-vectorization enabled in QtGui, the 32bit version of
    qPremultiply is faster than the 64bit version since it can be vectorized
    wider (4x on 128bit as opposed to 2x). Since all our important 64bit
    targets have SIMD, that makes the 64bit version pointless.
    
    Change-Id: I4e9070a3a3c8e2b54f17a95ba0aee0405cbb8ec9
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
    
    cregit: 9be87085d848c9615319d1be7d35a1f9963c3380 https://github.com/qt/qtbase/commit/9be87085d848c9615319d1be7d35a1f9963c3380

--- tests/benchmarks/gui/image/qimageconversion/tst_qimageconversion.cpp
+++ tests/benchmarks/gui/image/qimageconversion/tst_qimageconversion.cpp
@@ -599,6 +599,19 @@ name|QImage
 operator|::
 name|Format_ARGB4444_Premultiplied
 expr_stmt|;
+name|QTest
+operator|::
+name|newRow
+argument_list|(
+literal|"argb32 -> argb32pm"
+argument_list|)
+operator|<<
+name|argb32
+operator|<<
+name|QImage
+operator|::
+name|Format_ARGB32_Premultiplied
+expr_stmt|;
 block|}
 end_function
 begin_function
