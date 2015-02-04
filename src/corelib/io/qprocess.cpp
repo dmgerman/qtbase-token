32cd9deb92024b0a55bf25378b856abe63ca6363
Joerg Bornemann <joerg.bornemann@theqtcompany.com>
2015-02-04 15:12:36 +0100

    QProcess/Win: fix sporadic crash
    QProcessPrivate::_q_processDied() was potentially called twice if there
    was a slot connected to readyRead() that called processEvents(), because
    the processFinishedNotifier was still enabled when readyRead() was
    emitted.
    This led to a null pointer access in findExitCode.
    
    Change-Id: I4b796b81f050dc46bb9469602984accaa6ebfa28
    Task-number: QTBUG-33731
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
    
    cregit: df3ffeec15cd20c249cb6ffb2d235526a05722a5 https://github.com/qt/qtbase/commit/df3ffeec15cd20c249cb6ffb2d235526a05722a5

--- src/corelib/io/qprocess.cpp
+++ src/corelib/io/qprocess.cpp
@@ -2390,9 +2390,6 @@ directive|endif
 ifdef|#
 directive|ifdef
 name|Q_OS_WIN
-name|drainOutputPipes
-argument_list|()
-expr_stmt|;
 if|if
 condition|(
 name|processFinishedNotifier
@@ -2403,6 +2400,9 @@ name|setEnabled
 argument_list|(
 literal|false
 argument_list|)
+expr_stmt|;
+name|drainOutputPipes
+argument_list|()
 expr_stmt|;
 endif|#
 directive|endif
