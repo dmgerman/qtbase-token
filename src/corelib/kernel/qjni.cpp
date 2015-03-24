08a6cbfdc3ec59fe69f7b333883a9aeb03d2ce80
Christian Strømme <christian.stromme@theqtcompany.com>
2015-03-24 13:09:44 +0100

    Android: Only take ownership of threads we explicitly attach to.
    Previously we would take ownership of all threads when attempting to
    attach it to the VM, regardless if it was already attached or not, to
    avoid expensive re-attachments in our own code. The downside of this
    approach is that we might hijack threads the user wants to control
    themselves, and the later detach the thread while it still in use, e.g.,
    after the QApplication instance is destroyed.
    
    This change does not add any infrastructure to enable more advanced
    management of attached threads, so threads might still be hijacked if
    the user make a call directly or indirectly through the QJNI API's on
    a thread that's not attached.
    
    Task-number: QTBUG-45110
    Change-Id: I30f7faa2d8c2c35151e2ac8875ebf839bcd6c7c6
    Reviewed-by: Sergey Galin <s.galin@2gis.ru>
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@theqtcompany.com>
    
    cregit: 6b19eec63190c385a465c29b1f811214c6257034 https://github.com/qt/qtbase/commit/6b19eec63190c385a465c29b1f811214c6257034

--- src/corelib/kernel/qjni.cpp
+++ src/corelib/kernel/qjni.cpp
@@ -1022,8 +1022,10 @@ operator|::
 name|javaVM
 argument_list|()
 decl_stmt|;
-if|if
-condition|(
+specifier|const
+name|jint
+name|ret
+init|=
 name|vm
 operator|->
 name|GetEnv
@@ -1038,10 +1040,23 @@ name|jniEnv
 argument_list|,
 name|JNI_VERSION_1_6
 argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|ret
+operator|==
+name|JNI_OK
+condition|)
+comment|// Already attached
+return|return;
+if|if
+condition|(
+name|ret
 operator|==
 name|JNI_EDETACHED
 condition|)
 block|{
+comment|// We need to (re-)attach
 name|JavaVMAttachArgs
 name|args
 init|=
@@ -1069,13 +1084,6 @@ operator|!=
 name|JNI_OK
 condition|)
 return|return;
-block|}
-if|if
-condition|(
-operator|!
-name|jniEnv
-condition|)
-return|return;
 if|if
 condition|(
 operator|!
@@ -1084,6 +1092,7 @@ operator|->
 name|hasLocalData
 argument_list|()
 condition|)
+comment|// If we attached the thread we own it.
 name|jniEnvTLS
 operator|->
 name|setLocalData
@@ -1092,6 +1101,7 @@ operator|new
 name|QJNIEnvironmentPrivateTLS
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 end_constructor
 begin_function
