9350cde7123aa9b2a286b62b2065510fc0447511
David Faure <david.faure@kdab.com>
2015-03-30 17:12:02 +0200

    QTextTable/QTextDocumentLayout: remove dead code
    Change-Id: I3eb72a43129c58574036b6ca8c8c8413ca24b43a
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
    
    cregit: 5d7858452377c50032654b6ab4d8f743e9f5068a https://github.com/qt/qtbase/commit/5d7858452377c50032654b6ab4d8f743e9f5068a

--- src/gui/text/qtexttable.cpp
+++ src/gui/text/qtexttable.cpp
@@ -5900,16 +5900,6 @@ name|nCols
 return|;
 block|}
 end_function
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-begin_endif
-unit|void QTextTable::mergeCells(const QTextCursor&selection) { }
-endif|#
-directive|endif
-end_endif
 begin_comment
 comment|/*!     \fn QTextCursor QTextTable::rowStart(const QTextCursor&cursor) const      Returns a cursor pointing to the start of the row that contains the     given \a cursor.      \sa rowEnd() */
 end_comment
