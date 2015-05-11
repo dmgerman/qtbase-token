a902cc5865eb1e6a1ce6330548f33514791fb578
Thiago Macieira <thiago.macieira@intel.com>
2015-05-11 18:30:00 +0900

    Make qglobal.h complain if you use -fPIE
    Prior to Qt 5.4.2 (commit 36d6eb721e7d5997ade75e289d4088dc48678d0d), we
    allowed it, but now we need to enforce that it is not used. Note that
    -fPIE does define __PIC__, so we need this to catch the use of -fPIE.
    
    [ChangeLog][Important Behavior Changes] On x86 and x86-64 systems with
    ELF binaries (especially Linux), due to a new optimization in GCC 5.x in
    combination with a recent version of GNU binutils, compiling Qt
    applications with -fPIE is no longer enough. Applications now need to be
    compiled with the -fPIC option if Qt's option "reduce relocations" is
    active. Note that Clang is known to generate incompatible code even with
    -fPIC if the -flto option is active.
    
    Task-number: QTBUG-45755
    Change-Id: I66a35ce5f88941f29aa6ffff13dd210e0aa2728f
    Reviewed-by: Dmitry Shachnev <mitya57@gmail.com>
    Reviewed-by: Simon Hausmann <simon.hausmann@theqtcompany.com>
    
    cregit: 3eca75de67b3fd2c890715b30c7899cebc096fe9 https://github.com/qt/qtbase/commit/3eca75de67b3fd2c890715b30c7899cebc096fe9

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -5637,18 +5637,25 @@ argument_list|(
 name|__ELF__
 argument_list|)
 operator|&&
+operator|(
 operator|!
 name|defined
 argument_list|(
 name|__PIC__
 argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__PIE__
+argument_list|)
+operator|)
 end_if
 begin_error
 error|#
 directive|error
 literal|"You must build your code with position independent code if Qt was built with -reduce-relocations. "
 error|\
-literal|"Compile your code with -fPIC."
+literal|"Compile your code with -fPIC (-fPIE is not enough)."
 end_error
 begin_endif
 endif|#
