5f0804d4311b3ed2cb167137c4270056a8611771
Paul Olav Tvete <paul.tvete@theqtcompany.com>
2015-06-18 17:00:27 +0200

    Extend high-DPI manual test
    Add several new tests to verify that Qt behaves properly when
    high-DPI scaling is enabled. Add a generic framework for
    manual tests for good measure. This could be refactored and
    used for other manual tests later.
    
    Includes tests written by Morten and Friedemann.
    
    Task-number: QTBUG-46615
    Change-Id: Ib6762ec1454711e71f0c094b19015932b99e8d6d
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
    
    cregit: 62bd4f5852137276e354746737a88c06168c4b8b https://github.com/qt/qtbase/commit/62bd4f5852137276e354746737a88c06168c4b8b

---
+++ tests/manual/highdpi/dragwidget.h
@@ -0,0 +1,149 @@
+begin_unit
+begin_comment
+comment|/****************************************************************************  **  ** Copyright (C) 2015 The Qt Company Ltd.  ** Contact: http://www.qt.io/licensing/  **  ** This file is part of the test suite of the Qt Toolkit.  **  ** $QT_BEGIN_LICENSE:LGPL21$  ** Commercial License Usage  ** Licensees holding valid commercial Qt licenses may use this file in  ** accordance with the commercial license agreement provided with the  ** Software or, alternatively, in accordance with the terms contained in  ** a written agreement between you and The Qt Company. For licensing terms  ** and conditions see http://www.qt.io/terms-conditions. For further  ** information use the contact form at http://www.qt.io/contact-us.  **  ** GNU Lesser General Public License Usage  ** Alternatively, this file may be used under the terms of the GNU Lesser  ** General Public License version 2.1 or version 3 as published by the Free  ** Software Foundation and appearing in the file LICENSE.LGPLv21 and  ** LICENSE.LGPLv3 included in the packaging of this file. Please review the  ** following information to ensure the GNU Lesser General Public License  ** requirements will be met: https://www.gnu.org/licenses/lgpl.html and  ** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.  **  ** As a special exception, The Qt Company gives you certain additional  ** rights. These rights are described in The Qt Company LGPL Exception  ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.  **  ** $QT_END_LICENSE$  **  ****************************************************************************/
+end_comment
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|DRAGWIDGET_H
+end_ifndef
+begin_define
+DECL|macro|DRAGWIDGET_H
+define|#
+directive|define
+name|DRAGWIDGET_H
+end_define
+begin_include
+include|#
+directive|include
+file|<QWidget>
+end_include
+begin_include
+include|#
+directive|include
+file|<QBasicTimer>
+end_include
+begin_decl_stmt
+name|QT_BEGIN_NAMESPACE
+DECL|variable|QDragEnterEvent
+name|class
+name|QDragEnterEvent
+decl_stmt|;
+end_decl_stmt
+begin_decl_stmt
+DECL|variable|QDropEvent
+name|class
+name|QDropEvent
+decl_stmt|;
+end_decl_stmt
+begin_decl_stmt
+name|QT_END_NAMESPACE
+name|class
+name|DragWidget
+range|:
+name|public
+name|QWidget
+block|{
+name|public
+operator|:
+name|DragWidget
+argument_list|(
+argument|QString text = QString()
+argument_list|,
+argument|QWidget *parent =
+literal|0
+argument_list|)
+block|;
+name|protected
+operator|:
+name|void
+name|dragEnterEvent
+argument_list|(
+argument|QDragEnterEvent *event
+argument_list|)
+name|Q_DECL_OVERRIDE
+block|;
+name|void
+name|dragLeaveEvent
+argument_list|(
+argument|QDragLeaveEvent *event
+argument_list|)
+name|Q_DECL_OVERRIDE
+block|;
+name|void
+name|dropEvent
+argument_list|(
+argument|QDropEvent *event
+argument_list|)
+name|Q_DECL_OVERRIDE
+block|;
+name|void
+name|mousePressEvent
+argument_list|(
+argument|QMouseEvent *event
+argument_list|)
+name|Q_DECL_OVERRIDE
+block|;
+name|void
+name|dragMoveEvent
+argument_list|(
+argument|QDragMoveEvent * event
+argument_list|)
+name|Q_DECL_OVERRIDE
+block|;
+name|void
+name|paintEvent
+argument_list|(
+argument|QPaintEvent *
+argument_list|)
+name|Q_DECL_OVERRIDE
+block|;
+name|void
+name|timerEvent
+argument_list|(
+argument|QTimerEvent *event
+argument_list|)
+name|Q_DECL_OVERRIDE
+block|;
+name|void
+name|showEvent
+argument_list|(
+argument|QShowEvent *event
+argument_list|)
+name|Q_DECL_OVERRIDE
+block|;
+name|void
+name|hideEvent
+argument_list|(
+argument|QHideEvent *event
+argument_list|)
+name|Q_DECL_OVERRIDE
+block|;
+name|private
+operator|:
+name|QPoint
+name|dragPos
+block|;
+name|QPoint
+name|dropPos
+block|;
+name|QBasicTimer
+name|dragTimer
+block|;
+name|QBasicTimer
+name|dropTimer
+block|;
+name|QWidget
+operator|*
+name|otherWindow
+block|; }
+decl_stmt|;
+end_decl_stmt
+begin_endif
+endif|#
+directive|endif
+end_endif
+begin_comment
+comment|// DRAGWIDGET_H
+end_comment
+end_unit
