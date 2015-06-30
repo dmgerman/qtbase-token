1286e6eec2e2473747b46cfe23e09a1ebff941b2
Marc Mutz <marc.mutz@kdab.com>
2015-06-30 22:11:15 +0200

    QtPrintSupport: Use Q_NULLPTR instead of 0 in all public headers
    This is in preparation of adding -Wzero-as-null-pointer-constant (or similar)
    to the headers check.
    
    Task-number: QTBUG-45291
    Change-Id: I6f591a42a8e8ea471d27666eb1cbc77b572b3e0d
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: efeab107bfd631b9e372f4d69923f2f788919467 https://github.com/qt/qtbase/commit/efeab107bfd631b9e372f4d69923f2f788919467

--- src/printsupport/dialogs/qpagesetupdialog.h
+++ src/printsupport/dialogs/qpagesetupdialog.h
@@ -65,7 +65,7 @@ name|QWidget
 operator|*
 name|parent
 operator|=
-literal|0
+name|Q_NULLPTR
 argument_list|)
 block|;
 name|explicit
@@ -75,7 +75,7 @@ name|QWidget
 operator|*
 name|parent
 operator|=
-literal|0
+name|Q_NULLPTR
 argument_list|)
 block|;
 operator|~
--- src/printsupport/dialogs/qprintdialog.h
+++ src/printsupport/dialogs/qprintdialog.h
@@ -74,7 +74,7 @@ name|QWidget
 operator|*
 name|parent
 operator|=
-literal|0
+name|Q_NULLPTR
 argument_list|)
 block|;
 name|explicit
@@ -84,7 +84,7 @@ name|QWidget
 operator|*
 name|parent
 operator|=
-literal|0
+name|Q_NULLPTR
 argument_list|)
 block|;
 operator|~
