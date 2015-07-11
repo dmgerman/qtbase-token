1eec48fb524bc605bcf46e4c71c817974d7ca601
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-07-11 22:32:46 +0200

    eglfs_kms: Skip disconnected outputs
    For some reason VMware reports 8 outputs, 7 of them are disconnected
    and so they cause several errors.
    
    Skip disconnected outputs to avoid those errors.
    
    Change-Id: I5f9fa2ef38b916af9f9ae8b50fce9fc40c18bff3
    Reviewed-by: Laszlo Agocs <laszlo.agocs@theqtcompany.com>
    
    cregit: dceb424fe81ae29dde8e49a6dad300c547c8be26 https://github.com/qt/qtbase/commit/dceb424fe81ae29dde8e49a6dad300c547c8be26

--- src/plugins/platforms/eglfs/deviceintegration/eglfs_kms/qeglfskmsdevice.cpp
+++ src/plugins/platforms/eglfs/deviceintegration/eglfs_kms/qeglfskmsdevice.cpp
@@ -776,6 +776,33 @@ return|return
 name|Q_NULLPTR
 return|;
 block|}
+comment|// Skip disconnected output
+if|if
+condition|(
+name|configuration
+operator|==
+name|OutputConfigPreferred
+operator|&&
+name|connector
+operator|->
+name|connection
+operator|==
+name|DRM_MODE_DISCONNECTED
+condition|)
+block|{
+name|qCDebug
+argument_list|(
+name|qLcEglfsKmsDebug
+argument_list|)
+operator|<<
+literal|"Skipping disconnected output"
+operator|<<
+name|connectorName
+expr_stmt|;
+return|return
+name|Q_NULLPTR
+return|;
+block|}
 comment|// Get the current mode on the current crtc
 name|drmModeModeInfo
 name|crtc_mode
