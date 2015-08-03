b16f7f054967efd31f47c5b7a1ee23816585ec21
Frederik Gladhorn <frederik.gladhorn@theqtcompany.com>
2015-08-03 10:07:17 +0200

    Do not install headers for private classes
    When merging the accessibility plugin into the widgets library, the
    headers were just moved. They should have gotten the _p at that time.
    
    Task-number: QTBUG-47569
    Change-Id: I0a2290dae3a8187596e9d7541ccf69beeb603296
    Reviewed-by: Dimitar Dobrev
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 0552331d0e42e7b73f36e31ffd3cc511b50d2ec0 https://github.com/qt/qtbase/commit/0552331d0e42e7b73f36e31ffd3cc511b50d2ec0

--- src/widgets/accessible/qaccessiblewidgetfactory_p.h
+++ src/widgets/accessible/qaccessiblewidgetfactory_p.h
@@ -18,6 +18,36 @@ define|#
 directive|define
 name|QACCESSIBLEWIDGETFACTORY_H
 end_define
+begin_comment
+comment|//
+end_comment
+begin_comment
+comment|//  W A R N I N G
+end_comment
+begin_comment
+comment|//  -------------
+end_comment
+begin_comment
+comment|//
+end_comment
+begin_comment
+comment|// This file is not part of the Qt API.  It exists purely as an
+end_comment
+begin_comment
+comment|// implementation detail.  This header file may change from version to
+end_comment
+begin_comment
+comment|// version without notice, or even be removed.
+end_comment
+begin_comment
+comment|//
+end_comment
+begin_comment
+comment|// We mean it.
+end_comment
+begin_comment
+comment|//
+end_comment
 begin_function_decl
 name|QT_BEGIN_NAMESPACE
 name|QAccessibleInterface
