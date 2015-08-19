82b9b0b1686b5bcc6ce6f1a54c57c582e72a4ea0
Shawn Rutledge <shawn.rutledge@digia.com>
2015-08-19 15:22:53 +0200

    xcb: send QTabletEvent before QMouseEvent
    It was a regression: drawing applications which can handle both mouse
    and tablet events should not need to wait to see whether a mouse event
    will be followed by a corresponding tablet event.  The tablet event is
    preferred because it has all the tablet parameters (pressure etc.)
    Also, show the timestamp of each mouse event in the log message,
    which helps to verify the correspondence.
    
    Task-number: QTBUG-47702
    Change-Id: I9e832c19de935ec1243138eb4c1484284f61cbfc
    Reviewed-by: Laszlo Agocs <laszlo.agocs@theqtcompany.com>
    
    cregit: cc6a06632b26c4f60cb29f540af6eafc31ae3f02 https://github.com/qt/qtbase/commit/cc6a06632b26c4f60cb29f540af6eafc31ae3f02

--- src/plugins/platforms/xcb/qxcbwindow.cpp
+++ src/plugins/platforms/xcb/qxcbwindow.cpp
@@ -12999,9 +12999,13 @@ name|qCDebug
 argument_list|(
 name|lcQpaXInput
 argument_list|,
-literal|"XI2 mouse press, button %d"
+literal|"XI2 mouse press, button %d, time %d"
 argument_list|,
 name|button
+argument_list|,
+name|ev
+operator|->
+name|time
 argument_list|)
 expr_stmt|;
 name|conn
@@ -13042,9 +13046,13 @@ name|qCDebug
 argument_list|(
 name|lcQpaXInput
 argument_list|,
-literal|"XI2 mouse release, button %d"
+literal|"XI2 mouse release, button %d, time %d"
 argument_list|,
 name|button
+argument_list|,
+name|ev
+operator|->
+name|time
 argument_list|)
 expr_stmt|;
 name|conn
@@ -13085,11 +13093,15 @@ name|qCDebug
 argument_list|(
 name|lcQpaXInput
 argument_list|,
-literal|"XI2 mouse motion %d,%d"
+literal|"XI2 mouse motion %d,%d, time %d"
 argument_list|,
 name|event_x
 argument_list|,
 name|event_y
+argument_list|,
+name|ev
+operator|->
+name|time
 argument_list|)
 expr_stmt|;
 name|handleMotionNotifyEvent
