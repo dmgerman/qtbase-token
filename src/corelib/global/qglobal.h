4faf23ca3783f478582ac42beeb6a6e94cdbe0b1
Thiago Macieira <thiago.macieira@intel.com>
2015-09-17 18:17:40 -0700

    Move the official Qt version from qglobal.h to .qmake.conf
    It's easier to parse than qglobal.h. The objective is actually to have
    macros with parts of the version number, so the major or minor numbers
    could be used in other preprocessor macros.
    
    Change-Id: I42e7ef1a481840699a8dffff1404eda1dd5c308d
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
    
    cregit: 2d2cb6434f1d6e00f421c98b20467ff3f4388319 https://github.com/qt/qtbase/commit/2d2cb6434f1d6e00f421c98b20467ff3f4388319

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -32,13 +32,6 @@ include|#
 directive|include
 file|<stddef.h>
 end_include
-begin_define
-DECL|macro|QT_VERSION_STR
-define|#
-directive|define
-name|QT_VERSION_STR
-value|"5.6.0"
-end_define
 begin_comment
 comment|/*    QT_VERSION is (major<< 16) + (minor<< 8) + patch. */
 end_comment
@@ -47,7 +40,7 @@ DECL|macro|QT_VERSION
 define|#
 directive|define
 name|QT_VERSION
-value|0x050600
+value|QT_VERSION_CHECK(QT_VERSION_MAJOR, QT_VERSION_MINOR, QT_VERSION_PATCH)
 end_define
 begin_comment
 comment|/*    can be used like #if (QT_VERSION>= QT_VERSION_CHECK(4, 4, 0)) */
