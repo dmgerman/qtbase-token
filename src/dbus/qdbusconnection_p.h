9663e6b4445ef81947a670e1fb17b1a06342cee1
Thiago Macieira <thiago.macieira@intel.com>
2015-09-27 12:05:04 -0700

    QDBusConnection: Remove unused members
    I can't find any use, ever, of them.
    
    Change-Id: I42e7ef1a481840699a8dffff1407eb1a93b128a8
    Reviewed-by: Alex Blasche <alexander.blasche@theqtcompany.com>
    
    cregit: 7dc0f42e7813953aa9b423e40d4be8039e6cacaa https://github.com/qt/qtbase/commit/7dc0f42e7813953aa9b423e40d4be8039e6cacaa

--- src/dbus/qdbusconnection_p.h
+++ src/dbus/qdbusconnection_p.h
@@ -1907,20 +1907,6 @@ name|pendingCalls
 decl_stmt|;
 end_decl_stmt
 begin_decl_stmt
-name|QMutex
-name|callDeliveryMutex
-decl_stmt|;
-end_decl_stmt
-begin_decl_stmt
-name|QDBusCallDeliveryEvent
-modifier|*
-name|callDeliveryState
-decl_stmt|;
-end_decl_stmt
-begin_comment
-comment|// protected by the callDeliveryMutex mutex
-end_comment
-begin_decl_stmt
 name|bool
 name|anonymousAuthenticationAllowed
 decl_stmt|;
