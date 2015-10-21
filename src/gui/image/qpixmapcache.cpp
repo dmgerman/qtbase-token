23274db5b42249b25bc3faa1c67480d5ba71bd0f
Thiago Macieira <thiago.macieira@intel.com>
2015-10-21 08:44:10 -0700

    Prevent the pixmap cache from crashing if it's been destroyed
    In case the static destructor has already been run, make sure we don't
    crash. This shouldn't happen, but could happen if the QApplication
    destructor is run in a weird order (after the static destructors have
    begun running).
    
    That's not usually a case we'd fix (unsupported), but since this change
    improves the code and also avoids creating the pixmap cache if it hadn't
    been used up until this point, the change is a net benefit.
    
    Task-number: QTBUG-48709
    Change-Id: Ia505aece07bf4e13a1faffff140f3e119cfc773e
    Reviewed-by: Gunnar Sletta <gunnar@sletta.org>
    
    cregit: be926e412c9c4ec9ee77b49dbe370fbb5451f0e1 https://github.com/qt/qtbase/commit/be926e412c9c4ec9ee77b49dbe370fbb5451f0e1

--- src/gui/image/qpixmapcache.cpp
+++ src/gui/image/qpixmapcache.cpp
@@ -2319,8 +2319,14 @@ parameter_list|()
 block|{
 name|QT_TRY
 block|{
+if|if
+condition|(
 name|pm_cache
+operator|.
+name|exists
 argument_list|()
+condition|)
+name|pm_cache
 operator|->
 name|clear
 argument_list|()
