b470f8121c786f6b69be3ab14e515f32f0dffe69
Thiago Macieira <thiago.macieira@intel.com>
2015-10-22 12:54:45 -0700

    Move pointer size detection entirely to qprocessordetection.h
    This commit removes the legacy ptrsize check, which was deficient
    because it did not work for multiarch systems (when we supported fat
    OS X binaries) and did not work for bootstrap builds because the size
    might be different when cross-compiling.
    
    Instead, let's rely on the predefined preprocessor macros to detect
    correctly. As a nice side-effect, this fixes 64-bit Android builds
    cross-compiled from Windows.
    
    Task-number: QTBUG-48932
    Change-Id: I1d0f78915b5942aab07cffff140f9a52b9342f23
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
    Reviewed-by: Erik Verbruggen <erik.verbruggen@theqtcompany.com>
    
    cregit: 00f35b4ae7f94e0fe1c8c77efec401ab5f3c4855 https://github.com/qt/qtbase/commit/00f35b4ae7f94e0fe1c8c77efec401ab5f3c4855

--- src/corelib/global/qglobal.cpp
+++ src/corelib/global/qglobal.cpp
@@ -450,11 +450,6 @@ literal|"Qt assumes that char is 8 bits"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|QT_BOOTSTRAPPED
-end_ifndef
 begin_expr_stmt
 name|Q_STATIC_ASSERT_X
 argument_list|(
@@ -466,14 +461,10 @@ name|void
 operator|*
 argument_list|)
 argument_list|,
-literal|"configure test ptrsize failed to correctly determine QT_POINTER_SIZE"
+literal|"QT_POINTER_SIZE defined incorrectly"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
-begin_endif
-endif|#
-directive|endif
-end_endif
 begin_comment
 comment|/*!     \class QFlag     \inmodule QtCore     \brief The QFlag class is a helper data type for QFlags.      It is equivalent to a plain \c int, except with respect to     function overloading and type conversions. You should never need     to use this class in your applications.      \sa QFlags */
 end_comment
