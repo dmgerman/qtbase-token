6c9843d6476a2ada39908281dc87821bede5d018
Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
2015-11-23 14:14:46 +0100

    rewrite qtAddToolEnv()
    the primary purpose is making env var prepend mode work for unset
    variables on windows. this is achieved by using a conditional and delayed
    variable expansion. however, the latter is disabled by default and can
    be locally enabled only in batch files. therefore, write wrapper scripts
    and substitute them for the actual commands. we do this also on unix,
    both for consistency and simply because the commands look much less
    confusing.
    
    this change is slightly backwards-incompatible, as invoking
    qtAddToolEnv() multiple times on the same command will now make a total
    mess. also, invoking it on a command that contains 'make' macro
    expansions isn't a good idea, so testcase.prf needed an adjustment. the
    function is an undocumented internal, so Nobody Should Care (TM).
    
    this also reverts 80ebedecf9, as it's obsolete now.
    
    Change-Id: I8394b77868b495abcf27b688996ca74c40b80994
    Reviewed-by: Simon Hausmann <simon.hausmann@theqtcompany.com>
    
    cregit: 282f15feaae4c525602d537ab65cb61987eb5f7f https://github.com/qt/qtbase/commit/282f15feaae4c525602d537ab65cb61987eb5f7f

--- qmake/generators/win32/msvc_objectmodel.cpp
+++ qmake/generators/win32/msvc_objectmodel.cpp
@@ -12363,17 +12363,6 @@ name|Description
 operator|+=
 name|cmd_name
 expr_stmt|;
-comment|// Execute custom build steps in an environment variable scope to prevent unwanted
-comment|// side effects for downstream build steps
-name|CustomBuildTool
-operator|.
-name|CommandLine
-operator|+=
-name|QLatin1String
-argument_list|(
-literal|"setlocal"
-argument_list|)
-expr_stmt|;
 name|CustomBuildTool
 operator|.
 name|CommandLine
@@ -12386,15 +12375,6 @@ name|cmd
 operator|.
 name|trimmed
 argument_list|()
-argument_list|)
-expr_stmt|;
-name|CustomBuildTool
-operator|.
-name|CommandLine
-operator|+=
-name|QLatin1String
-argument_list|(
-literal|"endlocal"
 argument_list|)
 expr_stmt|;
 name|int
