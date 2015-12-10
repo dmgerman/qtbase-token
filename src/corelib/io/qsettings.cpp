762d3c47f91136968607e260791481535b477dd1
Andy Shaw <andy.shaw@digia.com>
2015-12-10 11:37:59 +0100

    Check if the file is writable even while QT_BOOTSTRAPPED
    QConfFile::isWritable() has the extra effect that it will try and
    create the path where the file should be if it does not already
    exist. So this cannot be omitted as 'qmake -set' may be used in a
    situation where the path does not yet exist.
    
    Change-Id: I0113644259f78d090a0687c44cf60d400be9c859
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
    
    cregit: 1440c13c8c1ed8eaa99c5d5edbd6fa61fd5eee8b https://github.com/qt/qtbase/commit/1440c13c8c1ed8eaa99c5d5edbd6fa61fd5eee8b

--- src/corelib/io/qsettings.cpp
+++ src/corelib/io/qsettings.cpp
@@ -7429,6 +7429,8 @@ literal|".lock"
 argument_list|)
 argument_list|)
 decl_stmt|;
+endif|#
+directive|endif
 if|if
 condition|(
 operator|!
@@ -7442,12 +7444,17 @@ name|confFile
 operator|->
 name|isWritable
 argument_list|()
+ifndef|#
+directive|ifndef
+name|QT_BOOTSTRAPPED
 operator|||
 operator|!
 name|lockFile
 operator|.
 name|lock
 argument_list|()
+endif|#
+directive|endif
 condition|)
 block|{
 name|setStatus
@@ -7460,8 +7467,6 @@ expr_stmt|;
 return|return;
 block|}
 block|}
-endif|#
-directive|endif
 comment|/*         We hold the lock. Let's reread the file if it has changed         since last time we read it.     */
 name|QFileInfo
 name|fileInfo
