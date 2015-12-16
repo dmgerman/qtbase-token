d68d0c12a642d7be1c81e041a345de4f77d8f1f3
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2015-12-16 14:50:35 +0100

    Use Q_CONSTRUCTOR_FUNCTION instead of doing the same manually
    Use the macro we have that does exactly what we want without the boiler
    plate code.
    
    Change-Id: I25c67a71f83f91f50128c8a54033ee53ad28e88a
    Reviewed-by: Erik Verbruggen <erik.verbruggen@theqtcompany.com>
    
    cregit: 879409fd0ffa2466dbd9f7274a4417b34ed18360 https://github.com/qt/qtbase/commit/879409fd0ffa2466dbd9f7274a4417b34ed18360

--- src/gui/painting/qdrawhelper.cpp
+++ src/gui/painting/qdrawhelper.cpp
@@ -45871,6 +45871,13 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+begin_function_decl
+specifier|extern
+name|void
+name|qInitBlendFunctions
+parameter_list|()
+function_decl|;
+end_function_decl
 begin_function
 DECL|function|qInitDrawhelperFunctions
 specifier|static
@@ -45878,6 +45885,10 @@ name|void
 name|qInitDrawhelperFunctions
 parameter_list|()
 block|{
+comment|// Set up basic blend function tables.
+name|qInitBlendFunctions
+argument_list|()
+expr_stmt|;
 ifdef|#
 directive|ifdef
 name|__SSE2__
@@ -47576,45 +47587,17 @@ directive|endif
 comment|// QT_COMPILER_SUPPORTS_MIPS_DSP || QT_COMPILER_SUPPORTS_MIPS_DSPR2
 block|}
 end_function
-begin_function_decl
-specifier|extern
-name|void
-name|qInitBlendFunctions
-parameter_list|()
-function_decl|;
-end_function_decl
-begin_class
-DECL|class|DrawHelperInitializer
-class|class
-name|DrawHelperInitializer
-block|{
-public|public:
-DECL|function|DrawHelperInitializer
-name|DrawHelperInitializer
-parameter_list|()
-block|{
-comment|// Set up basic blend function tables.
-name|qInitBlendFunctions
-argument_list|()
-expr_stmt|;
-comment|// Set up architecture optimized methods for the current machine.
-name|qInitDrawhelperFunctions
-argument_list|()
-expr_stmt|;
-block|}
-block|}
-class|;
-end_class
 begin_comment
 comment|// Ensure initialization if this object file is linked.
 end_comment
-begin_decl_stmt
-DECL|variable|drawHelperInitializer
-specifier|static
-name|DrawHelperInitializer
-name|drawHelperInitializer
-decl_stmt|;
-end_decl_stmt
+begin_expr_stmt
+DECL|variable|qInitDrawhelperFunctions
+name|Q_CONSTRUCTOR_FUNCTION
+argument_list|(
+name|qInitDrawhelperFunctions
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 begin_macro
 name|QT_END_NAMESPACE
 end_macro
