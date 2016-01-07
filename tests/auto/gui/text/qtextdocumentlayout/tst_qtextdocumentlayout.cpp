5ea2f6743bf4a9f3a12c3227fe4610f4288a8b77
Timur Pocheptsov <timur.pocheptsov@theqtcompany.com>
2016-01-07 09:03:09 +0100

    tst_qtextcocumentlayout::blockVisibility - make the test more robust
    The test inserts strings "0" ... "9" into the text document,
    takes the half of resulting document's size, makes half of
    lines invisible and compares sizes. On OS X 10.11 after inserting
    "4" the width changes, so making "4" invisible also reduces the width
    and QCOMPARE(currentSize, previosHalfSize) fails. Instead of digits,
    insert the same string "A" 10 times.
    
    Change-Id: Ie88a0442703f98949cea9bcdb694cecee59695f3
    Task-number: QTBUG-49848
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@theqtcompany.com>
    
    cregit: b530ca770f54165cd2766ccebb7269ba45bccb51 https://github.com/qt/qtbase/commit/b530ca770f54165cd2766ccebb7269ba45bccb51

--- tests/auto/gui/text/qtextdocumentlayout/tst_qtextdocumentlayout.cpp
+++ tests/auto/gui/text/qtextdocumentlayout/tst_qtextdocumentlayout.cpp
@@ -1554,12 +1554,7 @@ name|cursor
 operator|.
 name|insertText
 argument_list|(
-name|QString
-operator|::
-name|number
-argument_list|(
-name|i
-argument_list|)
+literal|"A"
 argument_list|)
 expr_stmt|;
 block|}
