72922ef4bf56cb5683b632f59f57365646e23b56
Edward Welbourne <edward.welbourne@theqtcompany.com>
2016-02-04 09:44:12 +0100

    Removed redundant and distracting test from conditional.
    If argumentTypes is NULL then it is indeed not equal to the address of
    a local static variable, so there is no need to follow the NULL-check
    with a check that it's not equal to that non-NULL address.
    
    Change-Id: I62362db747c0620b2195f7997368f026f535d57c
    Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>
    Reviewed-by: Jędrzej Nowacki <jedrzej.nowacki@theqtcompany.com>
    
    cregit: a054349be3ea8e68279ee3f478cc98e1ac522fe4 https://github.com/qt/qtbase/commit/a054349be3ea8e68279ee3f478cc98e1ac522fe4

--- src/corelib/kernel/qobject.cpp
+++ src/corelib/kernel/qobject.cpp
@@ -12112,11 +12112,6 @@ if|if
 condition|(
 operator|!
 name|argumentTypes
-operator|&&
-name|argumentTypes
-operator|!=
-operator|&
-name|DIRECT_CONNECTION_ONLY
 condition|)
 block|{
 name|QMetaMethod
