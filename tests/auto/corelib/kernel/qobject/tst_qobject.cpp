c4396293c7e37372a972464964dc1aec68ac683a
Maurice Kalinowski <maurice.kalinowski@theqtcompany.com>
2016-02-16 08:47:51 +0100

    winrt: Disable tests which connect to localhost
    WinRT does not allow do connect to the localhost due to security
    constraints and sandboxing. Hence we need to disable those
    currently.
    
    Change-Id: Idb8c71397a41e5fa5bad9d618dba1bb389e71b9c
    Reviewed-by: Oliver Wolff <oliver.wolff@theqtcompany.com>
    
    cregit: 6c7c34f219b6d156428d3fae79a09a2c68814ce6 https://github.com/qt/qtbase/commit/6c7c34f219b6d156428d3fae79a09a2c68814ce6

--- tests/auto/corelib/kernel/qobject/tst_qobject.cpp
+++ tests/auto/corelib/kernel/qobject/tst_qobject.cpp
@@ -13460,6 +13460,10 @@ name|wait
 argument_list|()
 expr_stmt|;
 block|}
+comment|// WinRT does not allow connection to localhost
+ifndef|#
+directive|ifndef
+name|Q_OS_WINRT
 block|{
 comment|// make sure socket notifiers are moved with the object
 name|MoveToThreadThread
@@ -13705,6 +13709,8 @@ name|wait
 argument_list|()
 expr_stmt|;
 block|}
+endif|#
+directive|endif
 block|}
 end_function
 begin_function
