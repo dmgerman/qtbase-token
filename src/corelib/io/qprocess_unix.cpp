f10f9f7f3f81770591ac7907391ce80a9b3325cf
Lorenz Haas <lorenz.haas@histomatics.de>
2016-03-25 12:19:03 +0100

    Fix debug message to match the method's name
    Change-Id: Ieaf7366663beeecec3240353d045fb19e2ae99ee
    Reviewed-by: Louai Al-Khanji <louai.al-khanji@theqtcompany.com>
    
    cregit: 215a04d51ac7e609588fc7b7f0d21aedf6140a87 https://github.com/qt/qtbase/commit/215a04d51ac7e609588fc7b7f0d21aedf6140a87

--- src/corelib/io/qprocess_unix.cpp
+++ src/corelib/io/qprocess_unix.cpp
@@ -3460,7 +3460,7 @@ name|QPROCESS_DEBUG
 argument_list|)
 name|qDebug
 argument_list|(
-literal|"QProcessPrivate::killProcess()"
+literal|"QProcessPrivate::terminateProcess()"
 argument_list|)
 expr_stmt|;
 endif|#
