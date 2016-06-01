1b1f8fdd25b909668cdb3ad0732a350797a40f13
Thiago Macieira <thiago.macieira@intel.com>
2016-06-01 09:58:48 +0000

    Revert "QObject::connect: reduce generated code size for statics and functors"
    This reverts commit 2e4191eadce4cfe944afd0baf37f06bbcb65c5f4. The change
    is backwards compatible but not forwards, so it can't appear past the .0
    release.
    
    Change-Id: I390c5e80795a9b3b27f6edcab79f7892a79c3564
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
    
    cregit: e81877218b2c5f5321a2032e602e7255f370e72a https://github.com/qt/qtbase/commit/e81877218b2c5f5321a2032e602e7255f370e72a

--- src/corelib/kernel/qobject_impl.h
+++ src/corelib/kernel/qobject_impl.h
@@ -1094,8 +1094,6 @@ argument_list|)
 block|{
 name|bool
 name|ret
-operator|=
-name|false
 block|;
 name|m_impl
 argument_list|(
@@ -1409,15 +1407,17 @@ break|break;
 case|case
 name|Compare
 case|:
+operator|*
+name|ret
+operator|=
+name|false
+expr_stmt|;
 comment|// not implemented
+break|break;
 case|case
 name|NumOperations
 case|:
-name|Q_UNUSED
-argument_list|(
-name|ret
-argument_list|)
-expr_stmt|;
+empty_stmt|;
 block|}
 block|}
 name|public
@@ -1545,15 +1545,17 @@ break|break;
 case|case
 name|Compare
 case|:
+operator|*
+name|ret
+operator|=
+name|false
+expr_stmt|;
 comment|// not implemented
+break|break;
 case|case
 name|NumOperations
 case|:
-name|Q_UNUSED
-argument_list|(
-name|ret
-argument_list|)
-expr_stmt|;
+empty_stmt|;
 block|}
 block|}
 name|public
